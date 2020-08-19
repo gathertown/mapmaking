ee3327b1c28762ae4d9ad0a2bc610e6824f110fd
Mark Carpenter <macarpenter@gmail.com>
2020-08-19 10:24:58 -0700

    Main Files
    Castle, Docks, Games, University

---
+++ tilesets/Gather Docks 0.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Gather Docks 0.x" tilewidth="32" tileheight="32" tilecount="1640" columns="40">
+ <image source="gather_docks_0.1.png" width="1280" height="1312"/>
+</tileset>
