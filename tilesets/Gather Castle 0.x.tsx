a020f7dee7e26f2db4cd61bfef9cd43351488d50
Mark Carpenter <macarpenter@gmail.com>
2020-08-19 17:10:36 -0700

    small changes

--- tilesets/Gather Castle 0.x.tsx
+++ tilesets/Gather Castle 0.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.5" name="Gather Castle 0.x" tilewidth="32" tileheight="32" tilecount="1920" columns="60">
- <image source="gather_castle_0.1.png" width="1920" height="1024"/>
+ <image source="gather_castle_0.2.png" width="1920" height="1024"/>
 </tileset>
