e66289a733b1b05657d86a38169c8f397bd362f7
Mark Carpenter <macarpenter@gmail.com>
2020-08-23 11:46:41 -0700

    Added Public (inside + outside)
    0.1

---
+++ tilesets/Gather Public Outside 0.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="Gather Public Outside 0.x" tilewidth="32" tileheight="32" tilecount="1920" columns="60">
+ <image source="gather_public_outside_0.1.png" width="1920" height="1024"/>
+</tileset>
