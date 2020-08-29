c5cd53153575b4af386e42cd23baf32a14ac4daa
Mark Carpenter <macarpenter@gmail.com>
2020-08-29 13:54:54 -0700

    Train Station 0.4.2

--- tilesets/Gather Train 0.x.tsx
+++ tilesets/Gather Train 0.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="Gather Train 0.x" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
- <image source="gather_train_0.4.1.png" width="1280" height="1280"/>
+ <image source="gather_train_0.4.2.png" width="1280" height="1280"/>
 </tileset>
