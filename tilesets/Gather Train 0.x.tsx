db4274360db341b90f5421afeeae07c37708ec9e
Mark Carpenter <macarpenter@gmail.com>
2020-08-28 17:59:54 -0700

    + tsx

--- tilesets/Gather Train 0.x.tsx
+++ tilesets/Gather Train 0.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Gather Train 0.x" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
- <image source="gather_train_0.3.png" width="1280" height="1280"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Train 0.x" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
+ <image source="gather_train_0.4.png" width="1280" height="1280"/>
 </tileset>
