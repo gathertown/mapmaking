fee6061aad87ffc5ce148d8314b40bbf7439deb3
Mark Carpenter <macarpenter@gmail.com>
2020-09-04 19:46:25 -0700

    Demo Conference

---
+++ maps/demo conference/Chalk.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Chalk" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="chalk.png" width="640" height="640"/>
+</tileset>
