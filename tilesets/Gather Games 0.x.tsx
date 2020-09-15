6571365e2a731bbf77d42c11fc11b27990100c45
Mark Carpenter <macarpenter@gmail.com>
2020-09-15 12:38:27 -0700

    new Furniture and Games tileset
    minor things

--- tilesets/Gather Games 0.x.tsx
+++ tilesets/Gather Games 0.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Gather Games 0.x" tilewidth="32" tileheight="32" tilecount="928" columns="29">
- <image source="gather_games_0.1.png" width="928" height="1024"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Games 0.x" tilewidth="32" tileheight="32" tilecount="928" columns="29">
+ <image source="gather_games_0.2.png" width="928" height="1024"/>
 </tileset>
