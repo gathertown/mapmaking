8061ae5554ad3cf48cb8bcb95457c6efeb3358a7
Mnoranth <aubreyotter@gmail.com>
2020-09-18 13:53:01 -0700

    created local train tile tsx

---
+++ maps/ranger station/gather_train_0.a.9.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="gather_train_0.a.9" tilewidth="32" tileheight="32" tilecount="4872" columns="87">
+ <image source="../../tilesets/gather_train_0.a.9.png" width="2800" height="1800"/>
+</tileset>
