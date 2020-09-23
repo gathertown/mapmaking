d3615f9366bdf6d6c691edd2fef02a5d306f7841
Mnoranth <aubreyotter@gmail.com>
2020-09-22 22:40:07 -0700

    ikea demo walkthrough
    this took WAY TOO LONG i hate it

---
+++ maps/ikea walk/gather_public_inside_0.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="gather_public_inside_0.1" tilewidth="32" tileheight="32" tilecount="928" columns="29">
+ <image source="../../tilesets/gather_public_inside_0.1.png" width="928" height="1024"/>
+</tileset>
