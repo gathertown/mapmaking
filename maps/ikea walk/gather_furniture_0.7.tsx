da1b5228e09194e5017c5aee0f7e94e40d9bdcd0
Mark Carpenter <macarpenter@gmail.com>
2020-09-24 10:27:41 -0700

    IKEA Walkthrough mark II
    might be able to slide this in before you update :)

--- maps/ikea walk/gather_furniture_0.7.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="gather_furniture_0.7" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../../tilesets/gather_furniture_0.7.png" width="1024" height="1024"/>
-</tileset>
