13ab81fbda97f7d838fc1b18a2e1660c454e66d7
Mark Carpenter <macarpenter@gmail.com>
2020-09-26 21:10:51 -0700

    Tileset updates

--- tilesets/Gather Docks 0.x.tsx
+++ tilesets/Gather Docks 0.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Gather Docks 0.x" tilewidth="32" tileheight="32" tilecount="1640" columns="40">
- <image source="gather_docks_0.1.png" width="1280" height="1312"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Docks 0.x" tilewidth="32" tileheight="32" tilecount="1280" columns="40">
+ <image source="gather_docks_0.2.png" width="1280" height="1024"/>
 </tileset>
