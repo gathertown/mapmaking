bf4ab25f1b3318878a96627b72c3fdb355a7ac54
Mark Carpenter <macarpenter@gmail.com>
2020-10-15 18:09:37 -0700

    floors and swatches
    at least to start

---
+++ tilesets/Gather Swatches 1.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Swatches 1.x" tilewidth="32" tileheight="32" tilecount="185" columns="5">
+ <image source="gather_swatches_1.0.png" width="160" height="1184"/>
+</tileset>
