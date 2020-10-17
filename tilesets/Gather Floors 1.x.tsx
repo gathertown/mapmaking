915c621fd9844fce66236dd1c579e534c9104093
Mark Carpenter <macarpenter@gmail.com>
2020-10-17 15:37:08 -0700

    start on Shadow. more Floors

--- tilesets/Gather Floors 1.x.tsx
+++ tilesets/Gather Floors 1.x.tsx
@@ -1,4 +1,41 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Gather Floors 1.x" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="gather_floors_1.0.png" width="1024" height="1024"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Floors 1.x" tilewidth="32" tileheight="32" tilecount="2560" columns="40">
+ <image source="gather_floors_1.0.png" width="1280" height="2048"/>
+ <terraintypes>
+  <terrain name="Blue Carpet" tile="-1"/>
+ </terraintypes>
+ <tile id="440" terrain=",,,0"/>
+ <tile id="441" terrain=",,0,0"/>
+ <tile id="442" terrain=",,0,0"/>
+ <tile id="443" terrain=",,0,0"/>
+ <tile id="444" terrain=",,0,"/>
+ <tile id="480" terrain=",0,,0"/>
+ <tile id="481" terrain="0,0,0,0"/>
+ <tile id="482" terrain="0,0,0,0"/>
+ <tile id="483" terrain="0,0,0,0"/>
+ <tile id="484" terrain="0,,0,"/>
+ <tile id="485" terrain=",0,0,0"/>
+ <tile id="486" terrain="0,0,0,0"/>
+ <tile id="487" terrain="0,,0,0"/>
+ <tile id="520" terrain=",0,,0"/>
+ <tile id="521" terrain="0,0,0,0"/>
+ <tile id="522" terrain="0,0,0,0"/>
+ <tile id="523" terrain="0,0,0,0"/>
+ <tile id="524" terrain="0,,0,"/>
+ <tile id="525" terrain="0,0,0,0"/>
+ <tile id="526" terrain="0,0,0,0"/>
+ <tile id="527" terrain="0,0,0,0"/>
+ <tile id="560" terrain=",0,,0"/>
+ <tile id="561" terrain="0,0,0,0"/>
+ <tile id="562" terrain="0,0,0,0"/>
+ <tile id="563" terrain="0,0,0,0"/>
+ <tile id="564" terrain="0,,0,"/>
+ <tile id="565" terrain="0,0,,0"/>
+ <tile id="566" terrain="0,0,0,0"/>
+ <tile id="567" terrain="0,0,0,"/>
+ <tile id="600" terrain=",0,,"/>
+ <tile id="601" terrain="0,0,,"/>
+ <tile id="602" terrain="0,0,,"/>
+ <tile id="603" terrain="0,0,,"/>
+ <tile id="604" terrain="0,,,"/>
 </tileset>
