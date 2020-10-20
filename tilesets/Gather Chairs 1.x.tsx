10b5cebc3242699c006097d0c05bb321c4d7c644
Mark Carpenter <macarpenter@gmail.com>
2020-10-19 22:06:56 -0700

    Added a pool map
    (and made tilesheets for tileset images)

---
+++ tilesets/Gather Chairs 1.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Chair 1.x" tilewidth="32" tileheight="32" tilecount="400" columns="25">
+ <image source="Gather Chairs 1.0.png" width="800" height="512"/>
+</tileset>
---
+++ tilesets/Gather Tables 1.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="775" columns="25">
+ <image source="Gather Tables 1.0.png" width="800" height="1020"/>
+</tileset>
