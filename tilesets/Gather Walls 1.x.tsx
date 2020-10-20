d481f5e8b3f6387d650434bd478f01e3fd3fed36
Mnoranth <aubreyotter@gmail.com>
2020-10-20 16:52:41 -0700

    Big bag o' walls

---
+++ tilesets/Gather Walls 1.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="walls" tilewidth="32" tileheight="32" tilecount="825" columns="25">
+ <image source="Gather Walls 1.0.png" width="800" height="1080"/>
+</tileset>
