7e894492aeb13abf9e470bef2a26f3619d33446c
Mark Carpenter <macarpenter@gmail.com>
2020-10-21 15:10:06 -0700

    tileset updates

---
+++ tilesets/Gather Games 1.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Games 1.x" tilewidth="32" tileheight="32" tilecount="600" columns="20">
+ <image source="Gather Games 1.0.png" width="640" height="960"/>
+</tileset>
