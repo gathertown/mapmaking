b25ecc6d37211d81e612604253d895a9153eefb2
Mark Carpenter <twostripe>
2020-10-26 12:04:20 -0700

    renamed stuff

--- tilesets/Gather Walls Interior 1.x.tsx
+++ tilesets/Gather Walls Interior 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="1000" columns="25">
- <image source="Gather Walls Interior 1.0.png" width="800" height="1280"/>
+<tileset version="1.2" tiledversion="1.3.2" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="1000" columns="25">
+ <image source="gather_walls_interior_1.0.png" width="800" height="1280"/>
 </tileset>
