d86fe9fca5fa2b4271d1c9ff78cc02b810a2415f
twostripe <macarpenter@gmail.com>
2020-10-29 12:45:05 -0700

    Tileset changes

--- tilesets/Gather Walls Old 1.x.tsx
+++ tilesets/Gather Walls Old 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="1080" columns="27">
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Walls Old 1.x" tilewidth="32" tileheight="32" tilecount="1080" columns="27">
  <image source="gather_walls_old_1.0.png" width="880" height="1280"/>
 </tileset>
