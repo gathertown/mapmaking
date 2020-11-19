3b4a18fef1b80bc7f1ec5991e41d3ad97f79c145
twostripe <macarpenter@gmail.com>
2020-11-18 17:53:03 -0800

    TripAdvisor office, started

---
+++ maps/TripAdvisor/TripAdvisor.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TripAdvisor" tilewidth="160" tileheight="64" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="160" height="64" source="ta-logo.png"/>
+ </tile>
+</tileset>
