6c09a8e6a265086f8e8d604762fd9f82503a02eb
twostripe <macarpenter@gmail.com>
2020-11-18 17:52:52 -0800

    new tilesets

--- tilesets/Gather Decoration 1.x.tsx
+++ tilesets/Gather Decoration 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="Gather Decoration 1.x" tilewidth="32" tileheight="32" tilecount="5760" columns="36">
- <image source="gather_decoration_1.1.png" width="1152" height="5120"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Gather Decoration 1.x" tilewidth="32" tileheight="32" tilecount="5760" columns="36">
+ <image source="gather_decoration_1.2.png" width="1152" height="5120"/>
 </tileset>
