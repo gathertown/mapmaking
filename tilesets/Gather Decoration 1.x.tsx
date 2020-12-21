923d6baadd6c6f0f9d2238b09f6f877a63e1d052
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2020-12-21 18:30:24 -0500

    Additional Decorations
    New Years Celebration Decorations, Kitchen Accessories
    
    Added:
    2 Sizes of triangle banner (one color variant)
    "Happy New Year" banner
    "2020" triangle banner (all one color variant)
    Champagne Bottle
    Champagne Glass
    Radio
    Stove
    Microwave Side View
    Oven Mitts
    Water Heater
    Cook Book
    Farmhouse Sink
    Trash Can
    Kitchen Cutting Board
    Kitchen Knife
    & found+cleaned up untidy Stray Pixels

--- tilesets/Gather Decoration 1.x.tsx
+++ tilesets/Gather Decoration 1.x.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Gather Decoration 1.x" tilewidth="32" tileheight="32" tilecount="5760" columns="36">
- <image source="gather_decoration_1.8.png" width="1152" height="5120"/>
+ <image source="gather_decoration_1.9.png" width="1152" height="5120"/>
  <tile id="1660">
   <animation>
    <frame tileid="1660" duration="250"/>
