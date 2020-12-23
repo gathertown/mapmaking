88d71a875f4f1097e663ed9290eea8ae8aa79af6
Daud alzayer <dsalzayer@gmail.com>
2020-12-23 15:11:16 -0500

    Platform enhancements
    - correct perspective on all wooden platforms
    - Gather Floors.tsx now includes terrain brushes for platforms
    
    Other stuff:
    -add new table asset
    -add new conference templates

--- tilesets/Gather Floors 1.x.tsx
+++ tilesets/Gather Floors 1.x.tsx
@@ -1,4 +1,78 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Gather Floors 1.x" tilewidth="32" tileheight="32" tilecount="2560" columns="40">
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Floors 1.x" tilewidth="32" tileheight="32" tilecount="2560" columns="40">
  <image source="gather_floors_1.0.png" width="1280" height="2048"/>
+ <terraintypes>
+  <terrain name="Dark wood platforms" tile="121"/>
+  <terrain name="Medium wood platforms" tile="129"/>
+  <terrain name="light wood platforms" tile="137"/>
+  <terrain name="Purple heart platforms" tile="145"/>
+ </terraintypes>
+ <tile id="80" terrain=",,,0"/>
+ <tile id="81" terrain=",,0,0"/>
+ <tile id="82" terrain=",,0,"/>
+ <tile id="88" terrain=",,,1"/>
+ <tile id="89" terrain=",,1,1"/>
+ <tile id="90" terrain=",,1,"/>
+ <tile id="96" terrain=",,,2"/>
+ <tile id="97" terrain=",,2,2"/>
+ <tile id="98" terrain=",,2,"/>
+ <tile id="104" terrain=",,,3"/>
+ <tile id="105" terrain=",,3,3"/>
+ <tile id="106" terrain=",,3,"/>
+ <tile id="120" terrain=",0,,0"/>
+ <tile id="121" terrain="0,0,0,0"/>
+ <tile id="122" terrain="0,,0,"/>
+ <tile id="128" terrain=",1,,1"/>
+ <tile id="129" terrain="1,1,1,1"/>
+ <tile id="130" terrain="1,,1,"/>
+ <tile id="136" terrain=",2,,2"/>
+ <tile id="137" terrain="2,2,2,2"/>
+ <tile id="138" terrain="2,,2,"/>
+ <tile id="144" terrain=",3,,3"/>
+ <tile id="145" terrain="3,3,3,3"/>
+ <tile id="146" terrain="3,,3,"/>
+ <tile id="160" terrain=",0,,"/>
+ <tile id="161" terrain="0,0,,"/>
+ <tile id="162" terrain="0,,,"/>
+ <tile id="168" terrain=",1,,"/>
+ <tile id="169" terrain="1,1,,"/>
+ <tile id="170" terrain="1,,,"/>
+ <tile id="176" terrain=",2,,"/>
+ <tile id="177" terrain="2,2,,"/>
+ <tile id="178" terrain="2,,,"/>
+ <tile id="184" terrain=",3,,"/>
+ <tile id="185" terrain="3,3,,"/>
+ <tile id="186" terrain="3,,,"/>
+ <tile id="200" terrain="0,0,0,"/>
+ <tile id="201" terrain="0,0,,"/>
+ <tile id="202" terrain="0,0,,0"/>
+ <tile id="208" terrain="1,1,1,"/>
+ <tile id="209" terrain="1,1,,"/>
+ <tile id="210" terrain="1,1,,1"/>
+ <tile id="216" terrain="2,2,2,"/>
+ <tile id="217" terrain="2,2,,"/>
+ <tile id="218" terrain="2,2,,2"/>
+ <tile id="224" terrain="3,3,3,"/>
+ <tile id="225" terrain="3,3,,"/>
+ <tile id="226" terrain="3,3,,3"/>
+ <tile id="240" terrain="0,,0,"/>
+ <tile id="242" terrain=",0,,0"/>
+ <tile id="248" terrain="1,,1,"/>
+ <tile id="250" terrain=",1,,1"/>
+ <tile id="256" terrain="2,,2,"/>
+ <tile id="258" terrain=",2,,2"/>
+ <tile id="264" terrain="3,,3,"/>
+ <tile id="266" terrain=",3,,3"/>
+ <tile id="280" terrain="0,,0,0"/>
+ <tile id="281" terrain=",,0,0"/>
+ <tile id="282" terrain=",0,0,0"/>
+ <tile id="288" terrain="1,,1,1"/>
+ <tile id="289" terrain=",,1,1"/>
+ <tile id="290" terrain=",1,1,1"/>
+ <tile id="296" terrain="2,,2,2"/>
+ <tile id="297" terrain=",,2,2"/>
+ <tile id="298" terrain=",2,2,2"/>
+ <tile id="304" terrain="3,,3,3"/>
+ <tile id="305" terrain=",,3,3"/>
+ <tile id="306" terrain=",3,3,3"/>
 </tileset>
