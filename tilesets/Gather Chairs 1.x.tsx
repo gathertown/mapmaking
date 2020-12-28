9e6c483d5767337692fd0f88930d1cb2268f8b99
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2020-12-28 13:50:39 -0500

    Couches and Armchairs update
    Larger couches, all views, all colorations added to Chairs tileset

--- tilesets/Gather Chairs 1.x.tsx
+++ tilesets/Gather Chairs 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Gather Chairs" tilewidth="32" tileheight="32" tilecount="400" columns="25">
- <image source="gather_chairs_1.0.png" width="800" height="512"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Chairs" tilewidth="32" tileheight="32" tilecount="650" columns="25">
+ <image source="gather_chairs_1.2.png" width="800" height="832"/>
 </tileset>
