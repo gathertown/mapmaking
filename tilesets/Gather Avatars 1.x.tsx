93b4d206b4b80eb7c3ba9dae40b3a0d8af81ab9a
Mark Carpenter <macarpenter@gmail.com>
2020-12-29 12:40:42 +0000

    new Avatar tileset (scaly sue and her friends)

---
+++ tilesets/Gather Avatars 1.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Avatars 1.x" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="gather_avatars_1.0.png" width="256" height="256"/>
+</tileset>
