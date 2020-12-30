e49ef736b170181ef3f518d1dcb3eadfac482ac1
Mark Carpenter <macarpenter@gmail.com>
2020-12-30 16:02:38 +0000

    beach updates
    (quick updates)

--- tilesets/Gather Floors 1.x.tsx
+++ tilesets/Gather Floors 1.x.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Gather Floors 1.x" tilewidth="32" tileheight="32" tilecount="2560" columns="40">
- <image source="gather_floors_1.0.png" width="1280" height="2048"/>
+ <image source="gather_floors_1.1.png" width="1280" height="2048"/>
  <terraintypes>
   <terrain name="Dark wood platforms" tile="121"/>
   <terrain name="Medium wood platforms" tile="129"/>
