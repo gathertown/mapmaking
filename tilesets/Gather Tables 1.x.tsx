2ccb66674d05085c68aa9ff905ca02fdcc562ddd
Mark Carpenter <macarpenter@gmail.com>
2021-01-03 01:01:24 +0000

    Walls Interior 1.4
    - added three wood/shed walls
    - added a new dirt to Floors

--- tilesets/Gather Tables 1.x.tsx
+++ tilesets/Gather Tables 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="806" columns="26">
- <image source="gather_tables_1.1.png" width="832" height="1020"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="806" columns="26">
+ <image source="gather_tables_1.2.png" width="832" height="1020"/>
 </tileset>
