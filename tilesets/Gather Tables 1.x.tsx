662317fd9fed7e9b5bb9edac6dde68868b7663d1
Mnoranth <aubreyotter@gmail.com>
2021-01-02 19:54:59 -0800

    hecking heck

--- tilesets/Gather Tables 1.x.tsx
+++ tilesets/Gather Tables 1.x.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<<<<<<< HEAD
 <tileset version="1.4" tiledversion="1.4.3" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="1066" columns="26">
  <image source="gather_tables_1.3.png" width="832" height="1340"/>
-=======
-<tileset version="1.4" tiledversion="1.4.3" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="806" columns="26">
- <image source="gather_tables_1.2.png" width="832" height="1020"/>
->>>>>>> 0286eac3df621f94dc1f6e89e5b0177a629f3506
 </tileset>
