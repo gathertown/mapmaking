4812cc05dc6dfadc1fbe14edec0e5595cf1b2487
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-01-03 15:58:37 -0500

    Added Red Brick Walls, Exit Signs
    Added Red Brick wall to bottom of interior walls tilesheet, added exit signs in red, green, single door and double door on the top far right of the tilesheet.

--- tilesets/Gather Walls Interior 1.x.tsx
+++ tilesets/Gather Walls Interior 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="2500" columns="25">
- <image source="gather_walls_interior_1.3.png" width="800" height="3200"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="2750" columns="25">
+ <image source="gather_walls_interior_1.4.png" width="800" height="3520"/>
 </tileset>
