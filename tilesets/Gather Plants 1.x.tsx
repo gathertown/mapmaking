9ed687194ba7141c610598343a1806e87f04c80d
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-01-06 10:23:53 -0500

    Replaced small tree in Plants, Deleted "dancing avatars for Jaden"
    Deleted "Dance For Jaden" folder in avatars, project they were needed for (for Jasmine) was finished.
    
    Updated file names for Plants.x and Tables.x, replaced small interior/urban tree. made it less spherical as well.

--- tilesets/Gather Plants 1.x.tsx
+++ tilesets/Gather Plants 1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Gather Plants 1.x" tilewidth="32" tileheight="32" tilecount="800" columns="20">
- <image source="gather_plants_1.1.png" width="640" height="1280"/>
+ <image source="gather_plants_1.2.png" width="640" height="1280"/>
 </tileset>
