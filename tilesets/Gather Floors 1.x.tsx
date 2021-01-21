8dee96d1665a33d3f062cf182eb22e9b7b95ba67
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-01-21 09:54:15 -0500

    Small edits (from last week!)
    Noticed 1.2 and 1.3 were co-existing, deleted 1.2 and redirected tileset file to 1.3

--- tilesets/Gather Floors 1.x.tsx
+++ tilesets/Gather Floors 1.x.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Gather Floors 1.x" tilewidth="32" tileheight="32" tilecount="2560" columns="40">
- <image source="gather_floors_1.2.png" width="1280" height="2048"/>
+ <image source="gather_floors_1.3.png" width="1280" height="2048"/>
  <terraintypes>
   <terrain name="Walnut Platform" tile="121"/>
   <terrain name="Oak Platform" tile="129"/>
