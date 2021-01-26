6fdff5aa5aff9f9cf0bf4f5fa9a34fd5a3cb2857
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-01-26 18:15:13 -0500

    Lunar New Year + Valentine's Day
    Added to the side of "seasonal/celebration" deco

--- tilesets/Gather Decoration 1.x.tsx
+++ tilesets/Gather Decoration 1.x.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Gather Decoration 1.x" tilewidth="32" tileheight="32" tilecount="6840" columns="36">
- <image source="gather_decoration_1.20.png" width="1152" height="6080"/>
+ <image source="gather_decoration_1.21.png" width="1152" height="6080"/>
  <terraintypes>
   <terrain name="tight wide fence" tile="2153"/>
   <terrain name="wide white fence" tile="2148"/>
