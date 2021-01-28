0c214afa948809241ef61995a023cc3f1710d7d7
Mnoranth <aubreyotter@gmail.com>
2021-01-28 13:25:57 -0800

    office stuff

---
+++ maps/Gather Office 4/junk drawer/skyline 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="skyline" tilewidth="32" tileheight="32" tilecount="75" columns="15">
+ <image source="../../../assets/Skyline1x.PNG" width="500" height="173"/>
+</tileset>
