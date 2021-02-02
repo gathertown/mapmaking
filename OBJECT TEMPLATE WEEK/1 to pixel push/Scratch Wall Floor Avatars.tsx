cfc81d97ba92aa0ea3b569d507ae7dec2489f88f
Mark Carpenter <macarpenter@gmail.com>
2021-02-01 17:21:15 -0800

    chairs -> to color
    and one -> to color QA

---
+++ OBJECT TEMPLATE WEEK/1 to pixel push/Scratch Wall Floor Avatars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Scratch Wall/Floor/Avatars" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="scratch_walls_floors_avatars.png" width="256" height="256"/>
+</tileset>
