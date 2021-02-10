13b4b1798f5f50f3c321cccd333d307f35b4ef01
Mnoranth <aubreyotter@gmail.com>
2021-02-10 15:54:17 -0800

    pipeline

---
+++ maps/Gather Office 4/junk drawer/Skyline HUGE.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Skyline HUGE" tilewidth="32" tileheight="32" tilecount="736" columns="46">
+ <image source="../../../assets/Skyline3x.PNG" width="1500" height="519"/>
+</tileset>
