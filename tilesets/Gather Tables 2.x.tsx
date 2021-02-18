f94e08ba1773db5e224b80865766f3e7f3584aa5
Mark Carpenter <macarpenter@gmail.com>
2021-02-18 11:18:58 -0800

    Tables 2.0

---
+++ tilesets/Gather Tables 2.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Tables 2.x" tilewidth="32" tileheight="32" tilecount="9000" columns="90">
+ <image source="gather_tables_2.0.png" width="2880" height="3200"/>
+</tileset>
