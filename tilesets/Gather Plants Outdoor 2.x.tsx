22c8364324615dea796a5aec50f2aa8124382758
Daud alzayer <dsalzayer@gmail.com>
2021-02-22 13:48:04 -0500

    plants 2.0

---
+++ tilesets/Gather Plants Outdoor 2.x.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Plants Outdoor 2.x" tilewidth="32" tileheight="32" tilecount="1760" columns="40">
+ <image source="gather_plants_outdoor_2.0.png" width="1280" height="1408"/>
+ <terraintypes>
+  <terrain name="Hedge (Round, outline only)" tile="1209"/>
+  <terrain name="Hedge (Round, fill)" tile="1168"/>
+  <terrain name="Hedge (Square, outline only)" tile="1449"/>
+  <terrain name="Hedge (Squre, fill)" tile="1408"/>
+ </terraintypes>
+ <tile id="1040" terrain=",,,0"/>
+ <tile id="1041" terrain=",,0,0"/>
+ <tile id="1042" terrain=",,0,"/>
+ <tile id="1043" terrain="0,0,0,"/>
+ <tile id="1044" terrain="0,0,,0"/>
+ <tile id="1080" terrain=",0,,0"/>
+ <tile id="1081" terrain="0,0,0,0"/>
+ <tile id="1082" terrain="0,,0,"/>
+ <tile id="1083" terrain="0,,0,0"/>
+ <tile id="1084" terrain=",0,0,0"/>
+ <tile id="1120" terrain=",0,,"/>
+ <tile id="1121" terrain="0,0,,"/>
+ <tile id="1122" terrain="0,,,"/>
+ <tile id="1160" terrain=",,,1"/>
+ <tile id="1161" terrain=",,1,1"/>
+ <tile id="1162" terrain=",,1,"/>
+ <tile id="1163" terrain="1,1,1,"/>
+ <tile id="1164" terrain="1,1,,1"/>
+ <tile id="1200" terrain=",1,,1"/>
+ <tile id="1201" terrain="1,1,1,1"/>
+ <tile id="1202" terrain="1,,1,"/>
+ <tile id="1203" terrain="1,,1,1"/>
+ <tile id="1204" terrain=",1,1,1"/>
+ <tile id="1240" terrain=",1,,"/>
+ <tile id="1241" terrain="1,1,,"/>
+ <tile id="1242" terrain="1,,,"/>
+ <tile id="1400" terrain=",,,3"/>
+ <tile id="1401" terrain=",,3,3"/>
+ <tile id="1402" terrain=",,3,"/>
+ <tile id="1403" terrain="3,3,3,"/>
+ <tile id="1404" terrain="3,3,,3"/>
+ <tile id="1440" terrain=",3,,3"/>
+ <tile id="1441" terrain="3,3,3,3"/>
+ <tile id="1442" terrain="3,,3,"/>
+ <tile id="1443" terrain="3,,3,3"/>
+ <tile id="1444" terrain=",3,3,3"/>
+ <tile id="1480" terrain=",3,,"/>
+ <tile id="1481" terrain="3,3,,"/>
+ <tile id="1482" terrain="3,,,"/>
+</tileset>
