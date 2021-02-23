a6d7b6d149bef98e9a409b8363bc390f1ff7b060
Mark Carpenter <macarpenter@gmail.com>
2021-02-23 08:33:36 -0800

    uploaded thumbnails, and space crates

---
+++ tilesets/Temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Temp" tilewidth="32" tileheight="32" tilecount="418" columns="11">
+ <image source="temp_diner.png" width="382" height="1216"/>
+</tileset>
