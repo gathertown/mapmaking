28a5487430773cdbd9b1951fa92eef5cc66a8b48
Mnoranth <aubreyotter@gmail.com>
2021-02-24 08:49:00 -0800

    Office

---
+++ maps/Gather Office 4/Desks 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Desks 2" tilewidth="32" tileheight="32" tilecount="546" columns="39">
+ <image source="../../OBJECT TEMPLATE WEEK/3 to assetize/leftovers (FINISHED)/desk.png" width="1248" height="448"/>
+</tileset>
---
+++ maps/Gather Office 4/Office chairs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Office chairs" tilewidth="32" tileheight="32" tilecount="90" columns="9">
+ <image source="../../OBJECT TEMPLATE WEEK/3 to assetize/leftovers (FINISHED)/chair_office_rolling.png" width="288" height="320"/>
+</tileset>
