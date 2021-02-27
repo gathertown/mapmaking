f165e69d325828b6d205b13168a8b62921aeee0d
Mnoranth <aubreyotter@gmail.com>
2021-02-26 21:04:10 -0800

    diner stuff

--- maps/Lounge/diner/Diner Walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Diner Walls" tilewidth="32" tileheight="32" tilecount="44" columns="11">
- <image source="../../../OBJECT TEMPLATE WEEK/4 to upload/wallsdiner1.png" width="382" height="128"/>
-</tileset>
--- maps/Lounge/diner/Diner tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Diner tileset " tilewidth="32" tileheight="32" tilecount="464" columns="16">
- <image source="../../../../scratch/diner tileset oops don't tell mark.png" width="512" height="928"/>
-</tileset>
