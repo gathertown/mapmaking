a2f9cc4fa856e775c4ca16b5d038d6ad75d37b5d
Mark Carpenter <macarpenter@gmail.com>
2021-03-03 17:14:29 -0800

    Drab Office walls

--- tilesets/Gather Walls Interior 1.x.tsx
+++ tilesets/Gather Walls Interior 1.x.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="3500" columns="25">
- <image source="gather_walls_interior_1.5.png" width="800" height="4480"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Walls Interior 1.x" tilewidth="32" tileheight="32" tilecount="5000" columns="25">
+ <image source="gather_walls_interior_1.6.png" width="800" height="6400"/>
  <terraintypes>
   <terrain name="White wall top" tile="427"/>
   <terrain name="White wall bottoms" tile="452"/>
