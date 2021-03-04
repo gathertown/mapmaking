d1f83c471048379e660dfb0dc8d4b00eaf8ae5a0
Mnoranth <aubreyotter@gmail.com>
2021-03-04 13:54:15 -0800

    diner medium

--- maps/Gather Office 4/Rugs too.tsx
+++ maps/Gather Office 4/Rugs too.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Rugs too" tilewidth="32" tileheight="32" tilecount="9750" columns="130">
- <image source="../../OBJECT TEMPLATE WEEK/3 to assetize/leftovers (FINISHED)/rugs.png" width="4160" height="2400"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Rugs too" tilewidth="32" tileheight="32" tilecount="13248" columns="138">
+ <image source="../../objects/WIP/leftovers (FINISHED)/rugs.png" width="4416" height="3072"/>
 </tileset>
