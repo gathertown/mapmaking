662287cc634fb0e39a98f6f7b56e609df9ea9d54
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-03-09 17:18:45 -0500

    Victorian Walls Exploration

--- tilesets/Temp.tsx
+++ tilesets/Temp.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Temp" tilewidth="32" tileheight="32" tilecount="874" columns="23">
- <image source="temp.png" width="764" height="1216"/>
+<tileset version="1.4" tiledversion="1.4.3" name="temp" tilewidth="32" tileheight="32" tilecount="912" columns="24">
+ <image source="temp.png" width="768" height="1216"/>
 </tileset>
