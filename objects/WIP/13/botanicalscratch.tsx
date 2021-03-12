9eddca83b567bc0bc76ac1548fbb0826f5f10aac
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-03-12 16:54:06 -0500

    lil fixes

---
+++ objects/WIP/13/botanicalscratch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="botanicalscratch" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="botanicalscratch.png" width="320" height="192"/>
+</tileset>
