448f59a816c027f8d5aad1239267a867b83fb94b
Mark Carpenter <macarpenter@gmail.com>
2021-03-15 18:03:55 -0700

    started re-organizing maps folder
    WIP

--- maps/Embark/Truck.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Truck" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="Embark Truck.png" width="256" height="192"/>
-</tileset>
