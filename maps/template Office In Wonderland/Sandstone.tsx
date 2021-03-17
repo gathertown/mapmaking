ab1e70ac63f6ed79f53bee09177239ffdd44f554
Longladle <72828341+Longladle@users.noreply.github.com>
2021-03-17 15:25:03 -0400

    Updates
    ^

---
+++ maps/template Office In Wonderland/Sandstone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Sandstone" tilewidth="32" tileheight="32" tilecount="56" columns="8">
+ <image source="../../../../Documents/Gather/Sandstone.png" width="256" height="224"/>
+</tileset>
