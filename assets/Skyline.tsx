bd921fd8f22964b797029d218fec6a0a294077ce
Mnoranth <aubreyotter@gmail.com>
2021-03-26 10:47:43 -0700

    moved skyline.tsx into mapmaking/assets
    also chopped up some tables

---
+++ assets/Skyline.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Skyline" tilewidth="32" tileheight="32" tilecount="736" columns="46">
+ <image source="../Adulthood is a myth/Work/Gather town/mapmaking/assets/Skyline3x.PNG" width="1500" height="519"/>
+</tileset>
---
+++ assets/skyline small (for behind windows).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="skyline smol" tilewidth="32" tileheight="32" tilecount="75" columns="15">
+ <image source="../Adulthood is a myth/Work/Gather town/mapmaking/assets/Skyline1x.PNG" width="500" height="173"/>
+</tileset>
