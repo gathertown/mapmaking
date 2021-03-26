fba8c08be1b825e375a75eee050372ed6e6f517f
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-03-26 17:18:59 -0400

    random

---
+++ objects/WIP/13/gather_plants_outdoor_2.0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="gather_plants_outdoor_2.0" tilewidth="32" tileheight="32" tilecount="1760" columns="40">
+ <image source="../../../tilesets/gather_plants_outdoor_2.0.png" width="1280" height="1408"/>
+</tileset>
