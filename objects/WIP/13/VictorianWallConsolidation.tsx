167508d9bcb555f159fd1e25269c641e3f4fd65e
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-03-26 14:49:12 -0400

    small edits on curtains, adjusting placements

---
+++ objects/WIP/13/VictorianWallConsolidation.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="VictorianWallConsolidation" tilewidth="32" tileheight="32" tilecount="627" columns="33">
+ <image source="Victorian/VictorianWallConsolidation.png" width="1056" height="608"/>
+</tileset>
