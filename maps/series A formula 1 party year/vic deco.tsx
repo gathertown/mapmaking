96fd4fd8db6ec735de7694cc5e15c14e830a3173
Mnoranth <aubreyotter@gmail.com>
2021-03-30 20:35:06 -0700

    series A updates

---
+++ maps/series A formula 1 party year/vic deco.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="vic deco" tilewidth="32" tileheight="32" tilecount="7125" columns="25">
+ <image source="temp.png" width="800" height="9120"/>
+</tileset>
