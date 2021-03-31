854fcb0f3018f0d7e5a21294b69204b45b3e21bc
Mnoranth <aubreyotter@gmail.com>
2021-03-31 14:01:33 -0700

    wyr

---
+++ maps/series A formula 1 party year/rugs beyond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rugs beyond" tilewidth="32" tileheight="32" tilecount="3840" columns="96">
+ <image source="../../../../../../Gather [Personal Projects]/rug_chinese_art_deco.png" width="3072" height="1280"/>
+</tileset>
