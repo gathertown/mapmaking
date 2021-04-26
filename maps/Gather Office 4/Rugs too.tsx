b465de50d1bf79ab5b4307388f3670b11cbbd403
Mnoranth <aubreyotter@gmail.com>
2021-04-26 02:38:10 -0700

    gather office changes (for better or for worse)

--- maps/Gather Office 4/Rugs too.tsx
+++ maps/Gather Office 4/Rugs too.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Rugs too" tilewidth="32" tileheight="32" tilecount="13248" columns="138">
- <image source="../../objects/WIP/leftovers (FINISHED)/rugs.png" width="4416" height="3072"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Rugs too" tilewidth="32" tileheight="32" tilecount="2520" columns="36">
+ <image source="../../objects/WIP/Rugs[5x5].png" width="1152" height="2240"/>
 </tileset>
