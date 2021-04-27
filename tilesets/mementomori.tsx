eafba0be2f6f8a4819cb825b5af8e73091aa2e4d
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-04-27 08:38:01 -0400

    Funeral Assets Uploaded

---
+++ tilesets/mementomori.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="mementomori" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../objects/WIP/13/mementomori.png" width="320" height="320"/>
+</tileset>
