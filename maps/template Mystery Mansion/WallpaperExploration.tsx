edfec6277f18133f791235790ff3e10cbde468e1
Longladle <brian@gather.town>
2021-04-30 15:11:06 -0400

    MM updates and some wood patterns
    ^

---
+++ maps/template Mystery Mansion/WallpaperExploration.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="WallpaperExploration" tilewidth="32" tileheight="32" tilecount="24000" columns="200">
+ <image source="../../tilesets/WallpaperExploration.png" width="6400" height="3840"/>
+</tileset>
