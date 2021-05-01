8a61541f1042dae2b4881f94f0ae76321eb59e81
Mnoranth <aubreyotter@gmail.com>
2021-04-30 21:34:23 -0700

    mystic library cont

--- maps/Mystic Library/Bookshelves.tsx
+++ maps/Mystic Library/Bookshelves.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Bookshelves " tilewidth="32" tileheight="32" tilecount="6688" columns="76">
- <image source="../../objects/uploaded/leftovers (FINISHED)/bookshelves.png" width="2432" height="2816"/>
+ <image source="../../objects/uploaded/_leftovers (FINISHED)/bookshelves.png" width="2432" height="2816"/>
 </tileset>
---
+++ maps/Mystic Library/Jaden's sky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Jaden's sky" tilewidth="32" tileheight="32" tilecount="483" columns="21">
+ <image source="../../swag/skyline/Skyline1x v6.png" width="680" height="750"/>
+</tileset>
