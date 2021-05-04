d2272822acf98fdb4a79dfd0dceebac9b0f0ab21
Longladle <brian@gather.town>
2021-05-04 14:21:14 -0400

    MM + Wall Updates
    Tweked a few px in the red/wood walls
    more walls exploration

---
+++ maps/template Mystery Mansion/rug_floral.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rug_floral" tilewidth="32" tileheight="32" tilecount="3552" columns="37">
+ <image source="../../objects/uploaded/_leftovers (FINISHED)/rug_floral.png" width="1184" height="3072"/>
+</tileset>
