cbce327572cb3fe50b2d0da9679651f5812478a0
Mark Carpenter <macarpenter@gmail.com>
2021-05-05 13:08:48 -0700

    started adding images for the style guide
    + a personal temp sheet

---
+++ objects/WIP/2S/Temp (Mark).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Temp (Mark)" tilewidth="32" tileheight="32" tilecount="1200" columns="20">
+ <image source="temp.png" width="640" height="1920"/>
+</tileset>
