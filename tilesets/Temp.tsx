bf1885826b0b1a1d57d1dbb71b96bf19a4056295
Daud Alzayer <dsalzayer@gmail.com>
2021-05-12 08:15:36 -0400

    various
    -updates to escape and islands
    -fixes to walls

--- tilesets/Temp.tsx
+++ tilesets/Temp.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="temp" tilewidth="32" tileheight="32" tilecount="7125" columns="25">
- <image source="temp.png" width="800" height="9120"/>
+<tileset version="1.4" tiledversion="1.4.3" name="temp" tilewidth="32" tileheight="32" tilecount="8550" columns="30">
+ <image source="temp.png" width="960" height="9120"/>
 </tileset>
