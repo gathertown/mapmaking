d2f2f93f2a9470b3479ac6704e2c351f090283d9
Mnoranth <aubreyotter@gmail.com>
2021-05-13 01:05:10 -0700

    office and some other stuff

---
+++ maps/Gather Office 4/Rugs too cont.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Rugs too cont." tilewidth="32" tileheight="32" tilecount="13248" columns="138">
+ <image source="../../objects/uploaded/_leftovers (FINISHED)/rugs.png" width="4416" height="3072"/>
+</tileset>
