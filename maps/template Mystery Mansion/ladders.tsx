a782728a00ce474828c9387ae4071be5de5b7db1
Longladle <brian@gather.town>
2021-05-14 09:42:54 -0400

    MM Changes
    ^

---
+++ maps/template Mystery Mansion/ladders.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="image (8)" tilewidth="32" tileheight="32" tilecount="21" columns="7">
+ <image source="image (8).png" width="224" height="96"/>
+</tileset>
