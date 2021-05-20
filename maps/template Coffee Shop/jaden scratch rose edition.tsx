1bc0bcfbde17072fe3e666b6b743438be5b33bb2
Mnoranth <aubreyotter@gmail.com>
2021-05-20 00:27:39 -0700

    Wow its midnight already

---
+++ maps/template Coffee Shop/jaden scratch rose edition.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="coffee shop stuff do not read :)" tilewidth="32" tileheight="32" tilecount="270" columns="18">
+ <image source="../../../../../../Gather [Personal Projects]/temp city stuff.png" width="576" height="480"/>
+</tileset>
