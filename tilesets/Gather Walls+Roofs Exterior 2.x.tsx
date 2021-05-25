f2f4d149e6945673c3320e72de6b8c1f67098a52
Mark Carpenter <macarpenter@gmail.com>
2021-05-25 13:18:01 -0700

    first walls/roofs tilesheets

---
+++ tilesets/Gather Walls+Roofs Exterior 2.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Walls+Roofs Exterior 2.x" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="gather_walls_exterior_2.a.png" width="640" height="640"/>
+</tileset>
