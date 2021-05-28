99ec598232baf34c03ce1321a791972afdb3ab63
Mnoranth <aubreyotter@gmail.com>
2021-05-28 16:11:53 -0700

    cluttering things up

---
+++ maps/template Coffee Shop/open mic/pride/pride 2 the reckoning.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="preide 2: the reckoning" tilewidth="32" tileheight="32" tilecount="308" columns="22">
+ <image source="../../../objects/WIP/JM/pride object template/prideobjectstemplate.png" width="704" height="448"/>
+</tileset>
---
+++ maps/template Coffee Shop/open mic/pride/pride.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="pride" tilewidth="32" tileheight="32" tilecount="180" columns="45">
+ <image source="../../../objects/WIP/JM/pride_bunting_flag_variants .png" width="1440" height="128"/>
+</tileset>
