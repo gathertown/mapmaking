54cda37189036600ea67b3a013dbc2d578bd5e58
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-06-03 10:51:54 -0400

    Building Blocks - Exterior environments, roof tests

---
+++ tilesets/BuldingBlocksTilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="BuldingBlocksTilesheet" tilewidth="32" tileheight="32" tilecount="1600" columns="40">
+ <image source="BuldingBlocksTilesheet.png" width="1280" height="1280"/>
+</tileset>
