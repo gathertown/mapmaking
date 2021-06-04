37b8f399293d772f0aa250d46dbdbb6ffbaaf595
Longladle <brian@gather.town>
2021-06-04 17:03:17 -0400

    Tiki Updates + Walls
    ^

---
+++ maps/template Beach Bar/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="walls" tilewidth="32" tileheight="32" tilecount="324" columns="27">
+ <image source="walls.png" width="864" height="384"/>
+</tileset>
