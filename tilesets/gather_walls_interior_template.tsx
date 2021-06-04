ace97bfb27f06c1b1cce6cfaa4f7b4037fcff2d0
Longladle <brian@gather.town>
2021-06-04 18:06:43 -0400

    More Wall Updates
    ^

---
+++ tilesets/gather_walls_interior_template.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="gather_walls_interior_template" tilewidth="32" tileheight="32" tilecount="3125" columns="25">
+ <image source="gather_walls_interior_template.png" width="800" height="4000"/>
+</tileset>
