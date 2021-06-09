ee135d7acb7d4bd425ed2545463133b3b9ec23e8
Mnoranth <aubreyotter@gmail.com>
2021-06-09 14:29:56 -0700

    unfurnished office et alii

---
+++ maps/template Botanica/Botanica.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="botanica tiles" tilewidth="32" tileheight="32" tilecount="182" columns="26">
+ <image source="../../objects/WIP/from zachary/botanical_garden.png" width="840" height="253"/>
+</tileset>
