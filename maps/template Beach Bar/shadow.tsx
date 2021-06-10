3d4ad1023240fae60b58c083b0249e8bf209c4a3
Longladle <brian@gather.town>
2021-06-10 12:47:37 -0400

    Tiki Thumbnails
    A bit of digital photography

---
+++ maps/template Beach Bar/shadow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="shadow" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="shadow.png" width="224" height="224"/>
+</tileset>
