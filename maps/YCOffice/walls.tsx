523baf58a5f15fbd268dc98e07203128d58678dc
Longladle <brian@gather.town>
2021-06-11 16:31:52 -0400

    YC and some minor walls fixes
    Found some px that were wrong in the white walls

---
+++ maps/YCOffice/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="walls" tilewidth="32" tileheight="32" tilecount="216" columns="24">
+ <image source="walls.png" width="768" height="288"/>
+</tileset>
