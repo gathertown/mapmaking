5b9a7ccea8d77e3c180d88dfbf37a20d6fb04af9
Longladle <brian@gather.town>
2021-06-15 17:21:38 -0400

    offices on offices
    Added fancy table to sheet

---
+++ maps/Lemontech/chair_small.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="chair_small" tilewidth="32" tileheight="32" tilecount="48" columns="12">
+ <image source="../../objects/uploaded/chair_small/chair_small.png" width="384" height="128"/>
+</tileset>
