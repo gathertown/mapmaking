b1fbfd36cd1a9f6e8a9dd2d12df478603f324a3f
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-06-21 17:10:37 -0400

    Facades and Signage progress continues!

---
+++ objects/WIP/13/Old Themes/gather_roofs_exterior_2.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="gather_roofs_exterior_2.a" tilewidth="32" tileheight="32" tilecount="12000" columns="150">
+ <image source="../../../../tilesets/gather_roofs_exterior_2.a.png" width="4800" height="2560"/>
+</tileset>
---
+++ objects/WIP/13/Old Themes/gather_walls_exterior_2.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="gather_walls_exterior_2.a" tilewidth="32" tileheight="32" tilecount="31200" columns="156">
+ <image source="../../../../tilesets/gather_walls_exterior_2.a.png" width="4992" height="6400"/>
+</tileset>
