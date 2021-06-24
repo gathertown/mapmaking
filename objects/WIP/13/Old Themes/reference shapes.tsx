5270b937e9d8f940d0a57b93caf94d212f56714d
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-06-24 15:58:26 -0400

    Edits to New Tilesheets, Style Guide Graphics and File Reorganization (from 13)
    13 Temp sheet cleared out, Edits to new Tilesheets. Midcentury Seating assets moved folders.

---
+++ objects/WIP/13/Old Themes/reference shapes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="reference shapes" tilewidth="32" tileheight="32" tilecount="520" columns="26">
+ <image source="../../../../references/reference shapes.png" width="832" height="640"/>
+</tileset>
