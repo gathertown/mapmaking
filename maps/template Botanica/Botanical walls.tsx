ee65cee353a246025384aa0ada27bf03e9214a6a
Mnoranth <aubreyotter@gmail.com>
2021-06-28 11:02:50 -0700

    my power is back on!

--- maps/template Botanica/Botanical walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="botanical walls" tilewidth="32" tileheight="32" tilecount="414" columns="18">
- <image source="../../objects/WIP/from zachary/botanical_garden_walls.png" width="576" height="736"/>
-</tileset>
