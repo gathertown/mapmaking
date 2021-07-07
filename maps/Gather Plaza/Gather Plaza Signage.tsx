3cbed7bd5755b7ae6707ec459ba86b1f1912f9c5
Mark Carpenter <macarpenter@gmail.com>
2021-07-07 13:43:31 -0700

    minor tilesheet edits + Gather Plaza initial

---
+++ maps/Gather Plaza/Gather Plaza Signage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Gather Plaza Signage" tilewidth="32" tileheight="32" tilecount="450" columns="30">
+ <image source="gp signage.png" width="960" height="480"/>
+</tileset>
