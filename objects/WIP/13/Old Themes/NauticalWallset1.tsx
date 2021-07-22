4e7e7d8b3ffedef4e4fb6b2463b3ea090c575a81
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-07-22 16:48:02 -0400

    Nautical Objects and Walls

---
+++ objects/WIP/13/Old Themes/NauticalWallset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="NauticalWallset1" tilewidth="32" tileheight="32" tilecount="225" columns="25">
+ <image source="../NauticalWallset1.png" width="800" height="288"/>
+</tileset>
