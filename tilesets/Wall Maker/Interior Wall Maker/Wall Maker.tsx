9023198d77d5f7682ce8bca8d5f92b7cf73df4ba
Daud Alzayer <dsalzayer@gmail.com>
2021-07-22 15:29:43 -0400

    Wall stuff

--- tilesets/Wall Maker/Interior Wall Maker/Wall Maker.tsx
+++ tilesets/Wall Maker/Interior Wall Maker/Wall Maker.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Wall Maker" tilewidth="32" tileheight="32" tilecount="675" columns="25">
- <image source="CHANGE THIS FOR NEW WALL.png" width="800" height="864"/>
+<tileset version="1.5" tiledversion="1.7.0" name="Wall Maker" tilewidth="32" tileheight="32" tilecount="700" columns="25">
+ <image source="CHANGE THIS FOR NEW WALL.png" width="800" height="912"/>
 </tileset>
