c58b12f88d93523a0d752a1f9c51a7ac86d88cc8
Mnoranth <aubreyotter@gmail.com>
2021-07-23 10:06:31 -0700

    stuff

---
+++ maps/custom (internal)/Gather Office 5/Rose palette.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Rose palette" tilewidth="32" tileheight="32" tilecount="625" columns="25">
+ <image source="../../../objects/WIP/Roz/working_palette.png" width="800" height="800"/>
+</tileset>
