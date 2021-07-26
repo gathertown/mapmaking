06ed15419fcde1d71078069580a4a1b5e8ba2672
Longladle <brian@gather.town>
2021-07-26 15:46:47 -0400

    Plaza Punt

---
+++ maps/custom (internal)/Gather Plaza/door2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="door2" tilewidth="32" tileheight="32" tilecount="48" columns="12">
+ <image source="door2.png" width="384" height="128"/>
+</tileset>
