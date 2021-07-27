ebdf72fe02f495f5d4d0208ccb799f4c99ad439c
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-07-27 17:57:59 -0400

    neon tests

---
+++ objects/WIP/13/Old Themes/Sprite-0037.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Sprite-0037" tilewidth="32" tileheight="32" tilecount="264" columns="12">
+ <image source="../../2S/neon/Sprite-0037.png" width="384" height="704"/>
+</tileset>
