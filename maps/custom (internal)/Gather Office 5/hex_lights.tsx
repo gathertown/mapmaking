dcf6e73733af0ec16b5b1ccce465f75d5717e6d4
Mnoranth <aubreyotter@gmail.com>
2021-07-30 05:02:29 -0700

    office updates

---
+++ maps/custom (internal)/Gather Office 5/hex_lights.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="hex lights" tilewidth="32" tileheight="32" tilecount="598" columns="23">
+ <image source="lights_neon_hexagonal.png" width="736" height="832"/>
+</tileset>
