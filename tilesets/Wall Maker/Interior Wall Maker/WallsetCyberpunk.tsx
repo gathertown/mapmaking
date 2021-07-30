189e12afc251f60dd40ba229c82494d42a4d1cce
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-07-30 15:10:05 -0400

    Wall Set Greeblies, Outlines

---
+++ tilesets/Wall Maker/Interior Wall Maker/WallsetCyberpunk.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="WallsetCyberpunk" tilewidth="32" tileheight="32" tilecount="225" columns="25">
+ <image source="../../../objects/WIP/13/WallsetCyberpunk.png" width="800" height="288"/>
+</tileset>
