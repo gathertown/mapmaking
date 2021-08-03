0207e39cd69d81dd10a3750f55de36d6438c1a84
Daud Alzayer <dsalzayer@gmail.com>
2021-08-03 11:39:02 -0400

    Pirate Office

---
+++ maps/templates/Pirate Office (Tiny)/pirateship.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="pirateship" tilewidth="32" tileheight="32" tilecount="450" columns="25">
+ <image source="pirateship.png" width="800" height="576"/>
+</tileset>
