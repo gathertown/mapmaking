7eb96a20fd9ad4760e3e3e22b3d1dc115b3bab82
Longladle <brian@gather.town>
2021-08-05 02:08:44 -0400

    Nautical Updates

---
+++ maps/templates/Bar Nautical/flooring.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="flooring" tilewidth="32" tileheight="32" tilecount="75" columns="15">
+ <image source="flooring.png" width="480" height="160"/>
+</tileset>
