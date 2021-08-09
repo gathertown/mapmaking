be510cb87df4a3a5e486000cdd4b5b538b50f74b
Sazdx <sazdxhikari@gmail.com>
2021-08-09 15:02:45 -0500

    relinked exterior decoration TSX

--- tilesets/gather_exterior_decoration_1.x.tsx
+++ tilesets/gather_exterior_decoration_1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="gather_deco_exterior_1.a" tilewidth="32" tileheight="32" tilecount="3600" columns="40">
- <image source="gather_decoration_exterior_1.1.png" width="1280" height="2880"/>
+<tileset version="1.2" tiledversion="1.3.0" name="gather_deco_exterior_1.a" tilewidth="32" tileheight="32" tilecount="5200" columns="40">
+ <image source="gather_decoration_exterior_1.2.png" width="1280" height="4160"/>
 </tileset>
