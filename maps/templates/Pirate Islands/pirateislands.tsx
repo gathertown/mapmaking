6a61d40bbebf70fc982fabc1e95267ff65ea6897
Daud Alzayer <dsalzayer@gmail.com>
2021-08-10 10:08:26 -0400

    Maps
    - Pirate Islands (Early WIP)
    - Cyberpunk Detective office

---
+++ maps/templates/Pirate Islands/pirateislands.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="pirateislands" tilewidth="32" tileheight="32" tilecount="1650" columns="25">
+ <image source="pirateislands.png" width="800" height="2112"/>
+</tileset>
