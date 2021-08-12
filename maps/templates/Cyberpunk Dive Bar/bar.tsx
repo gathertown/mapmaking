8bc8eb5456d775c45834d0147ef33d475751e516
Daud Alzayer <dsalzayer@gmail.com>
2021-08-12 09:40:50 -0400

    cyber maps
    - cybermaps cybermaps does whatever a cyber can
    - dive bar
    - detective office

---
+++ maps/templates/Cyberpunk Dive Bar/bar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="bar" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="divebar.png" width="128" height="128"/>
+</tileset>
