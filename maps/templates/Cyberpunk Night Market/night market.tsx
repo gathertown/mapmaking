bf9f8237e9c45585943291cc1a7cac77093e8e66
Daud Alzayer <dsalzayer@gmail.com>
2021-08-19 16:24:20 -0400

    Night Market
    - map, thumbnails etc
    - small change to floors

---
+++ maps/templates/Cyberpunk Night Market/night market.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="night market" tilewidth="32" tileheight="32" tilecount="200" columns="10">
+ <image source="cyberpunk city stuff.png" width="320" height="640"/>
+</tileset>
