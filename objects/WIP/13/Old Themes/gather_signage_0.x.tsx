99e968ca252ffcf2acb27ca1a54b114edff04242
Mnoranth <aubreyotter@gmail.com>
2021-08-24 10:41:28 -0700

    gather office crap

--- objects/WIP/13/Old Themes/gather_signage_0.x.tsx
+++ objects/WIP/13/Old Themes/gather_signage_0.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="gather_signage_0.1" tilewidth="32" tileheight="32" tilecount="1680" columns="40">
- <image source="../../../../tilesets/gather_signage_0.1.png" width="1280" height="1345"/>
+<tileset version="1.5" tiledversion="1.7.0" name="gather_signage_0.1" tilewidth="32" tileheight="32" tilecount="3160" columns="40">
+ <image source="../../../../tilesets/gather_signage_1.2.png" width="1280" height="2529"/>
 </tileset>
