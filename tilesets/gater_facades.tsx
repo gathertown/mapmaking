5ea25d968761b47e0d626c33d77ee742fc705dc9
Daud Alzayer <dsalzayer@gmail.com>
2021-08-28 10:41:37 -0400

    Maps push
    -Video map
    - Night Market
    -Sneaking in a fix to sideways stone stairs.

---
+++ tilesets/gater_facades.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="gater_facades" tilewidth="32" tileheight="32" tilecount="5256" columns="72">
+ <image source="gather_facade_elements_1.0.png" width="2304" height="2336"/>
+</tileset>
