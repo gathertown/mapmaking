6835c76a4dbfa751b82e2162865fff1263be3213
Mark Carpenter <macarpenter@gmail.com>
2021-09-08 17:08:05 -0700

    cityscape addition to the Vid Walkthrough map

---
+++ maps/custom (internal)/Events vid walkthrough/Cityscape.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Cityscape" tilewidth="32" tileheight="32" tilecount="525" columns="25">
+ <image source="cityscape.png" width="800" height="690"/>
+</tileset>
