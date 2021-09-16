747586f9959d16aa532d001f96daaeff05cb88ae
nostalgiapurgatory <76004312+nostalgiapurgatory@users.noreply.github.com>
2021-09-16 17:13:36 -0400

    Assetized Spooky objects

---
+++ objects/WIP/13/Halloween/Witch_Cottage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Witch_Cottage" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="Witch_Cottage.png" width="416" height="416"/>
+</tileset>
