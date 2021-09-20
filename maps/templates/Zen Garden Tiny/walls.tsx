ccd2a5624b856eb67c2f9d80fc4635f2c368209a
Longladle <brian@gather.town>
2021-09-20 18:35:09 -0400

    Tweaks, Beach, and more contraband roof tiles

---
+++ maps/templates/Zen Garden Tiny/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="walls" tilewidth="32" tileheight="32" tilecount="55" columns="11">
+ <image source="walls.png" width="352" height="160"/>
+</tileset>
