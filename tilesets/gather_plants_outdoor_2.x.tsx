40c3a94567237b84ea1a26aa72b22a359915fe69
Mnoranth <aubreyotter@gmail.com>
2021-09-20 15:46:17 -0700

    Gather Office!

--- tilesets/gather_plants_outdoor_2.x.tsx
+++ tilesets/gather_plants_outdoor_2.x.tsx
@@ -1,49 +1,51 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Gather Plants Outdoor 2.x" tilewidth="32" tileheight="32" tilecount="1760" columns="40">
- <image source="gather_plants_outdoor_2.0.png" width="1280" height="1408"/>
- <terraintypes>
-  <terrain name="Hedge (Round, outline only)" tile="1209"/>
-  <terrain name="Hedge (Round, fill)" tile="1168"/>
-  <terrain name="Hedge (Square, outline only)" tile="1449"/>
-  <terrain name="Hedge (Squre, fill)" tile="1408"/>
- </terraintypes>
- <tile id="1040" terrain=",,,0"/>
- <tile id="1041" terrain=",,0,0"/>
- <tile id="1042" terrain=",,0,"/>
- <tile id="1043" terrain="0,0,0,"/>
- <tile id="1044" terrain="0,0,,0"/>
- <tile id="1080" terrain=",0,,0"/>
- <tile id="1081" terrain="0,0,0,0"/>
- <tile id="1082" terrain="0,,0,"/>
- <tile id="1083" terrain="0,,0,0"/>
- <tile id="1084" terrain=",0,0,0"/>
- <tile id="1120" terrain=",0,,"/>
- <tile id="1121" terrain="0,0,,"/>
- <tile id="1122" terrain="0,,,"/>
- <tile id="1160" terrain=",,,1"/>
- <tile id="1161" terrain=",,1,1"/>
- <tile id="1162" terrain=",,1,"/>
- <tile id="1163" terrain="1,1,1,"/>
- <tile id="1164" terrain="1,1,,1"/>
- <tile id="1200" terrain=",1,,1"/>
- <tile id="1201" terrain="1,1,1,1"/>
- <tile id="1202" terrain="1,,1,"/>
- <tile id="1203" terrain="1,,1,1"/>
- <tile id="1204" terrain=",1,1,1"/>
- <tile id="1240" terrain=",1,,"/>
- <tile id="1241" terrain="1,1,,"/>
- <tile id="1242" terrain="1,,,"/>
- <tile id="1400" terrain=",,,3"/>
- <tile id="1401" terrain=",,3,3"/>
- <tile id="1402" terrain=",,3,"/>
- <tile id="1403" terrain="3,3,3,"/>
- <tile id="1404" terrain="3,3,,3"/>
- <tile id="1440" terrain=",3,,3"/>
- <tile id="1441" terrain="3,3,3,3"/>
- <tile id="1442" terrain="3,,3,"/>
- <tile id="1443" terrain="3,,3,3"/>
- <tile id="1444" terrain=",3,3,3"/>
- <tile id="1480" terrain=",3,,"/>
- <tile id="1481" terrain="3,3,,"/>
- <tile id="1482" terrain="3,,,"/>
+<tileset version="1.5" tiledversion="1.7.0" name="Gather Plants Outdoor 2.x" tilewidth="32" tileheight="32" tilecount="4760" columns="40">
+ <image source="gather_plants_outdoor_2.1.png" width="1280" height="3808"/>
+ <wangsets>
+  <wangset name="Terrains" type="corner" tile="-1">
+   <wangcolor name="Hedge (Round, outline only)" color="#ff0000" tile="1209" probability="1"/>
+   <wangcolor name="Hedge (Round, fill)" color="#00ff00" tile="1168" probability="1"/>
+   <wangcolor name="Hedge (Square, outline only)" color="#0000ff" tile="1449" probability="1"/>
+   <wangcolor name="Hedge (Squre, fill)" color="#ff7700" tile="1408" probability="1"/>
+   <wangtile tileid="1040" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1041" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="1042" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="1043" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="1044" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="1080" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="1081" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1082" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="1083" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="1084" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="1120" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="1121" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="1122" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="1160" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="1161" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="1162" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="1163" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="1164" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="1200" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="1201" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1202" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="1203" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="1204" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="1240" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="1241" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="1242" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="1400" wangid="0,0,0,4,0,0,0,0"/>
+   <wangtile tileid="1401" wangid="0,0,0,4,0,4,0,0"/>
+   <wangtile tileid="1402" wangid="0,0,0,0,0,4,0,0"/>
+   <wangtile tileid="1403" wangid="0,4,0,0,0,4,0,4"/>
+   <wangtile tileid="1404" wangid="0,4,0,4,0,0,0,4"/>
+   <wangtile tileid="1440" wangid="0,4,0,4,0,0,0,0"/>
+   <wangtile tileid="1441" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="1442" wangid="0,0,0,0,0,4,0,4"/>
+   <wangtile tileid="1443" wangid="0,0,0,4,0,4,0,4"/>
+   <wangtile tileid="1444" wangid="0,4,0,4,0,4,0,0"/>
+   <wangtile tileid="1480" wangid="0,4,0,0,0,0,0,0"/>
+   <wangtile tileid="1481" wangid="0,4,0,0,0,0,0,4"/>
+   <wangtile tileid="1482" wangid="0,0,0,0,0,0,0,4"/>
+  </wangset>
+ </wangsets>
 </tileset>
