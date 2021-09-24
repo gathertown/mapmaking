4e7ae2482da5098141867a5367088fb3bc7fe1de
Daud Alzayer <dsalzayer@gmail.com>
2021-09-24 15:49:40 -0400

    Office and experience maps
    - new space flow
    - RW tutorial
    - Movienight
    - Racetrack
    - Spectrum
    - Speed Gathering
    - Tetris Tournament

---
+++ maps/custom (internal)/New Space Flow/Creation Chamber.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Creation Chamber" tilewidth="32" tileheight="32" tilecount="88" columns="22">
+ <image source="creationchamber.png" width="704" height="128"/>
+</tileset>
