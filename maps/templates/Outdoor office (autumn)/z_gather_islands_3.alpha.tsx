e69c6d8b8c13dc7884bff1a90a7bdb3e39565185
Daud Alzayer <dsalzayer@gmail.com>
2021-09-30 20:10:31 -0400

    Moops
    -Nexus
    -Movie Night
    -Autumn Office

---
+++ maps/templates/Outdoor office (autumn)/z_gather_islands_3.alpha.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="z_gather_islands_3.alpha" tilewidth="32" tileheight="32" tilecount="480" columns="20">
+ <image source="../../../tilesets/gather_islands_3.alpha.png" width="640" height="768"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="243">
+   <wangcolor name="" color="#ff0000" tile="243" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="183" probability="1"/>
+   <wangtile tileid="13" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="33" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="34" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="183" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="184" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="185" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="186" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="187" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="188" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="204" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="205" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="206" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="207" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="208" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="224" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="225" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="226" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="243" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="244" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="245" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="246" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="264" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="266" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="284" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="285" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="286" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
