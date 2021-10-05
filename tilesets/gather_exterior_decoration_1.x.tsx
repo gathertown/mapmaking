a8edea1406aed3af51b923f06b5d23ca4b3595a3
Sazdx <sazdxhikari@gmail.com>
2021-10-05 14:36:40 -0500

    updated Gather Decor Exterior tsx and png

--- tilesets/gather_exterior_decoration_1.x.tsx
+++ tilesets/gather_exterior_decoration_1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Gather Decoration Exterior" tilewidth="32" tileheight="32" tilecount="5200" columns="40">
- <image source="gather_decoration_exterior_1.2.png" width="1280" height="4160"/>
+<tileset version="1.2" tiledversion="1.3.0" name="Gather Decoration Exterior" tilewidth="32" tileheight="32" tilecount="5200" columns="40">
+ <image source="gather_decoration_exterior_1.3.png" width="1280" height="4160"/>
 </tileset>
