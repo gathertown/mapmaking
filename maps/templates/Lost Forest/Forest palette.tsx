5fde1219e54425e8ab3ed1d4de6fa228e97ad40e
Mnoranth <aubreyotter@gmail.com>
2021-10-12 12:11:06 -0700

    lost forest tests

---
+++ maps/templates/Lost Forest/Forest palette.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Forest palette" tilewidth="32" tileheight="32" tilecount="280" columns="14">
+ <image source="lost forest working palette.png" width="448" height="640"/>
+</tileset>
