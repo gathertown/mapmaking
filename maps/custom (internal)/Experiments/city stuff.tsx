976c678ec3eeb6735bc31a69d04f3c1da7ae219e
Daud Alzayer <dsalzayer@gmail.com>
2021-10-18 16:02:33 -0400

    experemente

---
+++ maps/custom (internal)/Experiments/city stuff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="city stuff" tilewidth="32" tileheight="32" tilecount="120" columns="20">
+ <image source="city stuff.png" width="640" height="192"/>
+</tileset>
