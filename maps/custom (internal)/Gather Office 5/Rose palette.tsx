b657ad7e5dac830af106f436a847912d0e0b1bd5
Mnoranth <aubreyotter@gmail.com>
2021-10-18 12:33:41 -0700

    file moving

--- maps/custom (internal)/Gather Office 5/Rose palette.tsx
+++ maps/custom (internal)/Gather Office 5/Rose palette.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Rose palette" tilewidth="32" tileheight="32" tilecount="625" columns="25">
- <image source="../../../objects/WIP/Roz/working_palette.png" width="800" height="800"/>
+<tileset version="1.5" tiledversion="1.7.0" name="Rose palette" tilewidth="32" tileheight="32" tilecount="1125" columns="25">
+ <image source="working_palette.png" width="800" height="1470"/>
 </tileset>
