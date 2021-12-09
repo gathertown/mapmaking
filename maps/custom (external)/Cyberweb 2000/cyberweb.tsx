86f441b799ff54b95b64f926ce834bb137fe43b8
Daud Alzayrt <dsalzayer@gmail.com>
2021-12-09 18:04:05 -0500

    misc
    -add cyberweb
    -add partner's showroom

---
+++ maps/custom (external)/Cyberweb 2000/cyberweb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="cyberweb" tilewidth="32" tileheight="32" tilecount="200" columns="10">
+ <image source="cyberweb.png" width="320" height="640"/>
+</tileset>
