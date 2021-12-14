bee0d61ac44185669f1d5536cefbb3fd9eee0b60
Mnoranth <aubreyotter@gmail.com>
2021-12-14 11:01:11 -0800

    moved palette over to folder

---
+++ maps/templates/Winter Gathering/temp sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="temp sheet" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="temp sheet.png" width="640" height="640"/>
+</tileset>
