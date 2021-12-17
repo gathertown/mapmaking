7144e5e4eb062800b1fa47ae024b934a2ea72c7c
Daud Alzayrt <dsalzayer@gmail.com>
2021-12-17 15:56:03 -0500

    partner's showroom update

---
+++ maps/custom (internal)/Parners Showroom/starstileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="starstileset" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="starstileset.png" width="320" height="320"/>
+</tileset>
