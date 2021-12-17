448dd918d0e2bc5c1ad9477a9486586385fa46f4
Longladle <brian@gather.town>
2021-12-17 16:42:20 -0500

    Arrr Dubbb

---
+++ maps/custom (internal)/Metaverse Cantina/Walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Walls" tilewidth="32" tileheight="32" tilecount="693" columns="33">
+ <image source="Walls.png" width="1056" height="672"/>
+</tileset>
---
+++ maps/custom (internal)/Metaverse Cantina/actual walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="actual walls" tilewidth="32" tileheight="32" tilecount="528" columns="24">
+ <image source="actual walls.png" width="768" height="704"/>
+</tileset>
