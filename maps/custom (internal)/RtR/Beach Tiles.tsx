6cba06bcd47d2f14aae0985d63e160cdef44a876
Daud Alzayrt <dsalzayer@gmail.com>
2022-01-04 09:29:53 -0500

    RtR Maps

---
+++ maps/custom (internal)/RtR/Beach Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="_Beach Tiles" tilewidth="32" tileheight="32" tilecount="300" columns="15">
+ <image source="beach_tiles.png" width="480" height="640"/>
+</tileset>
---
+++ maps/custom (internal)/RtR/Maze Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="_maze" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="maze_tiles.png" width="512" height="320"/>
+</tileset>
