dc8f6e1623b732c9f3f5bf55eb7ce85ccc271f87
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-01-05 16:51:07 -0500

    Updated Tables tilesheet

--- tilesets/gather_tables_1.x.tsx
+++ tilesets/gather_tables_1.x.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="1066" columns="26">
- <image source="gather_tables_1.7.png" width="832" height="1340"/>
+<tileset version="1.5" tiledversion="1.7.2" name="Gather Tables 1.x" tilewidth="32" tileheight="32" tilecount="9000" columns="90">
+ <image source="gather_tables_2.1.png" width="2880" height="3200"/>
 </tileset>
