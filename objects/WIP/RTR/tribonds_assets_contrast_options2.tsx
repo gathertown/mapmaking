1c042a03420471b8a0e653db1f63fbbd697e89c3
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-01-05 16:11:06 -0500

    Sci Fi Car and RTR Assets

---
+++ objects/WIP/RTR/tribonds_assets_contrast_options2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tribonds_assets_contrast_options2" tilewidth="32" tileheight="32" tilecount="225" columns="9">
+ <image source="tribonds_assets_contrast_options2.png" width="288" height="800"/>
+</tileset>
