bce16d2aeafcfc46a6cd00fb0e51870ea33385a9
Daud Alzayrt <dsalzayer@gmail.com>
2022-01-07 15:54:10 -0500

    Oh look, more RtR

---
+++ maps/custom (internal)/RtR/_lab.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="_lab" tilewidth="32" tileheight="32" tilecount="1320" columns="12">
+ <image source="lab_tiles.png" width="384" height="360"/>
+</tileset>
