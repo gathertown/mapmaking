47abfc613e6b15addee544a0d44f011484b98f93
Longladle <brian@gather.town>
2022-02-01 18:02:58 -0500

    Walkthrough

---
+++ maps/custom (internal)/Walkthrough General/temp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="temp" tilewidth="32" tileheight="32" tilecount="567" columns="21">
+ <image source="temp.png" width="672" height="864"/>
+</tileset>
