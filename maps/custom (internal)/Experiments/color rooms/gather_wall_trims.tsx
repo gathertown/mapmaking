49e644289476ac0eebab808d0df032993064efe6
Daud Alzayrt <dsalzayer@gmail.com>
2022-02-08 14:48:18 -0500

    grab bag
    - wall expereiments update
    - RtR updates
    - etc

---
+++ maps/custom (internal)/Experiments/color rooms/gather_wall_trims.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="trimsanddoors" tilewidth="32" tileheight="32" tilecount="600" columns="15">
+ <image source="../../../../objects/WIP/DZ/Walls/trimsanddoors.png" width="480" height="1280"/>
+</tileset>
