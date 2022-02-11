f4974802f3670691f48266dbee1f05a86f63bf49
Concave Operations <operations@concave.lol>
2022-02-10 22:54:31 -0500

    Deploy Odyssey Showcase

---
+++ concaveWorld/Tilesets (32x32)/Cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Cave" tilewidth="32" tileheight="32" tilecount="80" columns="8">
+ <image source="cave.png" width="256" height="336"/>
+</tileset>
---
+++ concaveWorld/Tilesets (32x32)/Dungeon_A4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Dungeon_A4" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="Dungeon_A4.png" width="512" height="480"/>
+</tileset>
