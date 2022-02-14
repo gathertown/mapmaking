e87820f985426ffc2c2534b1f5bf63da5a489ed2
twostripe <macarpenter@gmail.com>
2022-02-14 14:57:16 -0800

    Gather Office - draft 5 (coworking)
    - new coworking tables

--- maps/custom (internal)/Gather Office 5/Rose palette.tsx
+++ maps/custom (internal)/Gather Office 5/Rose palette.tsx
@@ -1,4 +1,64 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Rose palette" tilewidth="32" tileheight="32" tilecount="1125" columns="25">
- <image source="working_palette.png" width="800" height="1470"/>
+<tileset version="1.5" tiledversion="1.7.2" name="Rose palette" tilewidth="32" tileheight="32" tilecount="2875" columns="25">
+ <image source="working_palette.png" width="800" height="3708"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="Stone Purple (Raised)" color="#ff0000" tile="1928" probability="1"/>
+   <wangcolor name="Stone Purple (Flush)" color="#00ff00" tile="1982" probability="1"/>
+   <wangcolor name="Stone Grey (Raised)" color="#0000ff" tile="2003" probability="1"/>
+   <wangcolor name="Stone Grey (Flush)" color="#ff7700" tile="2057" probability="1"/>
+   <wangtile tileid="1925" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1926" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="1927" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="1928" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="1929" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="1930" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="1931" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="1932" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="1933" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="1934" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="1950" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="1951" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1952" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="1953" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="1954" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="1955" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="1956" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1957" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="1958" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="1959" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="1975" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="1976" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="1977" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="1980" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="1981" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="1982" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="2000" wangid="0,0,0,3,0,0,0,0"/>
+   <wangtile tileid="2001" wangid="0,0,0,3,0,3,0,0"/>
+   <wangtile tileid="2002" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="2003" wangid="0,3,0,0,0,3,0,3"/>
+   <wangtile tileid="2004" wangid="0,3,0,3,0,0,0,3"/>
+   <wangtile tileid="2005" wangid="0,0,0,4,0,0,0,0"/>
+   <wangtile tileid="2006" wangid="0,0,0,4,0,4,0,0"/>
+   <wangtile tileid="2007" wangid="0,0,0,0,0,4,0,0"/>
+   <wangtile tileid="2008" wangid="0,4,0,0,0,4,0,4"/>
+   <wangtile tileid="2009" wangid="0,4,0,4,0,0,0,4"/>
+   <wangtile tileid="2025" wangid="0,3,0,3,0,0,0,0"/>
+   <wangtile tileid="2026" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="2027" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="2028" wangid="0,0,0,3,0,3,0,3"/>
+   <wangtile tileid="2029" wangid="0,3,0,3,0,3,0,0"/>
+   <wangtile tileid="2030" wangid="0,4,0,4,0,0,0,0"/>
+   <wangtile tileid="2031" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="2032" wangid="0,0,0,0,0,4,0,4"/>
+   <wangtile tileid="2033" wangid="0,0,0,4,0,4,0,4"/>
+   <wangtile tileid="2034" wangid="0,4,0,4,0,4,0,0"/>
+   <wangtile tileid="2050" wangid="0,3,0,0,0,0,0,0"/>
+   <wangtile tileid="2051" wangid="0,3,0,0,0,0,0,3"/>
+   <wangtile tileid="2052" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="2055" wangid="0,4,0,0,0,0,0,0"/>
+   <wangtile tileid="2056" wangid="0,4,0,0,0,0,0,4"/>
+   <wangtile tileid="2057" wangid="0,0,0,0,0,0,0,4"/>
+  </wangset>
+ </wangsets>
 </tileset>
