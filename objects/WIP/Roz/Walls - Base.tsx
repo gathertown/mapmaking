439246c6815c1fba4ae7493942d0fe97f53eda1a
Mnoranth <aubreyotter@gmail.com>
2022-02-16 18:08:07 -0800

    5 days of mapmaking that I forgot to push heck yea
    all drafting work for Talk Rooms

--- objects/WIP/Roz/Walls - Base.tsx
+++ objects/WIP/Roz/Walls - Base.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Walls - Base" tilewidth="32" tileheight="32" tilecount="1824" columns="24">
- <image source="walls_allwall.png" width="768" height="2432"/>
+<tileset version="1.5" tiledversion="1.7.2" name="Walls - Base" tilewidth="32" tileheight="32" tilecount="1920" columns="24">
+ <image source="walls_allwall.png" width="768" height="2560"/>
 </tileset>
