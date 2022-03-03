1f8b1e1ca67ad38df4c4eed3d52de8dcd5b9bf24
Longladle <brian@gather.town>
2022-03-03 18:18:01 -0500

    Poster Rooms, Tutorial, and Foyer

---
+++ maps/templates/Talk Rooms/Event Entrance (redux)/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="floor" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="floor.png" width="160" height="160"/>
+</tileset>
