907ed0814a7b14d7c939ab01f45521cbe9b154d2
Longladle <brian@gather.town>
2022-03-11 17:13:52 -0500

    RW Expoeriences

---
+++ tilesets/Walls/allwall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="allwall" tilewidth="32" tileheight="32" tilecount="1824" columns="24">
+ <image source="allwall.png" width="768" height="2432"/>
+</tileset>
