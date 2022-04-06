728649b24e6c50ef3a628cdda481e067f89f5162
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-04-06 17:50:52 -0400

    Floor explorations

---
+++ objects/WIP/13/a TEMP folder/newfloorexplore.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="newfloorexplore" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="newfloorexplore.png" width="1024" height="1024"/>
+</tileset>
---
+++ objects/WIP/13/a TEMP folder/portaltest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="portaltest" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="portaltest.png" width="256" height="128"/>
+</tileset>
