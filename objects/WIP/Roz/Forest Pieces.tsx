5d35c5c36d319d71f531bd42fc5d29f4906d8ecc
Mnoranth <aubreyotter@gmail.com>
2022-05-08 16:28:26 -0700

    gettin wild

---
+++ objects/WIP/Roz/Forest Pieces.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Forest Pieces" tilewidth="32" tileheight="32" tilecount="880" columns="40">
+ <image source="forests etc.png" width="1280" height="720"/>
+</tileset>
