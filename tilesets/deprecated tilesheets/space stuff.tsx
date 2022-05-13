9e69ac1c47d75711ecaffc79b8d175856e80a6bc
David Laffranchi <seii@macbookpro-wifi.jiyuu-ni.net>
2022-05-12 20:09:07 -0600

    Return of the space station!
    Re-linked space station map to proper graphics

---
+++ tilesets/deprecated tilesheets/space stuff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="space stuff" tilewidth="32" tileheight="32" tilecount="300" columns="10">
+ <image source="../../objects/WIP/13/maybe deprecated/space stuff.png" width="320" height="960"/>
+</tileset>
---
+++ tilesets/deprecated tilesheets/space tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="space tiles" tilewidth="32" tileheight="32" tilecount="288" columns="12">
+ <image source="../../objects/WIP/13/maybe deprecated/space tiles.png" width="384" height="768"/>
+</tileset>
