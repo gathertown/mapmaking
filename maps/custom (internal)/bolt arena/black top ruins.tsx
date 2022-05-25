066d230dfe024082aa304ae189993e4b5a0bc118
Daud Alzayrt <dsalzayer@gmail.com>
2022-05-25 10:28:45 -0400

    DA maps
    Office tweak
    bolt arena add

---
+++ maps/custom (internal)/bolt arena/black top ruins.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="black top ruins" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="Assets/black top ruins.png" width="128" height="160"/>
+</tileset>
--- tilesets/WindowsObjects.tsx
+++ tilesets/WindowsObjects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="WindowsObjects" tilewidth="32" tileheight="32" tilecount="320" columns="16">
- <image source="../WindowsObjects.png" width="512" height="640"/>
+ <image source="WindowsObjects.png" width="512" height="640"/>
 </tileset>
