c1271e1ed63538a82a8ddd00ef4d7081c992981f
Longladle <brian@gather.town>
2022-05-27 11:36:56 -0400

    RWE Galore

---
+++ maps/custom (internal)/1-10/numbers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="numbers" tilewidth="32" tileheight="32" tilecount="120" columns="10">
+ <image source="numbers.png" width="320" height="384"/>
+</tileset>
---
+++ tilesets/Floors/TileAndStone.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="TileAndStone" tilewidth="32" tileheight="32" tilecount="1944" columns="36">
+ <image source="TileAndStone.png" width="1152" height="1728"/>
+</tileset>
