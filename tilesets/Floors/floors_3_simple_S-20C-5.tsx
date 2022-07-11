a0354fa422b5538dbbce9e776e49b31785f7c319
Sazdx <sazdxhikari@gmail.com>
2022-07-11 14:57:58 -0500

    Changed Benches, chairs, diner booths, and the tilesheets for the simple and complex floors

---
+++ tilesets/Floors/floors_3_simple_S-20C-5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floors_3_simple_S-20C-5" tilewidth="32" tileheight="32" tilecount="6572" columns="124">
+ <image source="floors_3_simple_S-20C-5.png" width="3968" height="1696"/>
+</tileset>
