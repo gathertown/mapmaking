63f65c1f87e0d2999393c385a033c9b25cedc467
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-07-11 16:37:42 -0400

    Re-routing complex and simple floors
    Moves old floors tilesheets and tiled files to the deprecated tilesheets folder, and re establishes the correct two files to pull floors information from as a source of truth!

---
+++ tilesets/floors_3_complex_S-20C-5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floors_3_complex_S-20C-5" tilewidth="32" tileheight="32" tilecount="17856" columns="186">
+ <image source="Floors/floors_3_complex_S-20C-5.png" width="5952" height="3072"/>
+</tileset>
--- tilesets/gather_floors_2_exploration.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="gather_floors_2_exploration" tilewidth="32" tileheight="32" tilecount="8160" columns="60">
- <image source="deprecated tilesheets/gather_floors_2_exploration.png" width="1920" height="4352"/>
-</tileset>
