e2dbd3c2bd26b1985e8c316e314f3dda95303981
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-07-22 16:13:53 -0400

    wallsv3 and grafitti
    adds grafitti to tilesheet folder
    improvements to walls v3 (unstable)

---
+++ objects/WIP/13/a TEMP folder/Wall_unstable.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Wall_unstable" tilewidth="32" tileheight="32" tilecount="13632" columns="96">
+ <image source="../../../../tilesets/Walls/Wall_unstable.png" width="3072" height="4560"/>
+</tileset>
