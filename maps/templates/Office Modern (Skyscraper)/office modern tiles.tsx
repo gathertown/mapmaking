79d06633076a943b47771154179678716935b363
Daud Alzayrt <dsalzayer@gmail.com>
2022-07-25 16:57:56 -0400

    screenshots

---
+++ maps/templates/Office Modern (Skyscraper)/office modern tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="office modern tiles" tilewidth="32" tileheight="32" tilecount="143" columns="11">
+ <image source="office modern tiles.png" width="352" height="416"/>
+</tileset>
