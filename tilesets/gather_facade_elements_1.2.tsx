8becdbd1ee2cc236cd8fcde01b4aeeef9f71e5ff
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-08-08 14:00:10 -0400

    adds a new threshold building and consolidates facade elements, exterior decoration

---
+++ tilesets/gather_facade_elements_1.2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="gather_facade_elements_1.2" tilewidth="32" tileheight="32" tilecount="12456" columns="72">
+ <image source="gather_facade_elements_1.2.png" width="2304" height="5536"/>
+</tileset>
