dd398b4d98e8f5abf73a9034f65e805c222248c2
Daud Alzayrt <dsalzayer@gmail.com>
2022-08-17 12:12:46 -0400

    dimensional assets

---
+++ maps/custom (internal)/Advanced Tutorial Island/assets/_space tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="_space tiles" tilewidth="32" tileheight="32" tilecount="288" columns="12">
+ <image source="space tiles.png" width="384" height="768"/>
+</tileset>
