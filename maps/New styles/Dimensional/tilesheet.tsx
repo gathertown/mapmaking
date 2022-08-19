57036c46bcd0a258ddce12d7fb32244b417ae58f
Daud Alzayrt <dsalzayer@gmail.com>
2022-08-19 14:23:31 -0400

    Dimensional continues
    - disorganized dump of more blender files and images

---
+++ maps/New styles/Dimensional/tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tilesheet" tilewidth="32" tileheight="32" tilecount="198" columns="18">
+ <image source="tilesheet.png" width="576" height="352"/>
+</tileset>
