4fd9e0d50bab47f8ed4bd565c629d55301ca3065
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-10-04 11:47:52 -0400

    adds short desks

---
+++ maps/custom (internal)/RtR/RtR_Master_Tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="RtR_Master_Tilesheet" tilewidth="32" tileheight="32" tilecount="2425" columns="25">
+ <image source="RtR_Master_Tilesheet.png" width="800" height="3126"/>
+</tileset>
