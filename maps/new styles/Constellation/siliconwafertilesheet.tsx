75c2845b6ea9f40c68ffd18967f7eea8c1c88066
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-10-06 14:27:39 -0400

    adds new gather office and new constellation maps

---
+++ maps/new styles/Constellation/siliconwafertilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="siliconwafertilesheet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="siliconwafertilesheet.png" width="320" height="320"/>
+</tileset>
---
+++ objects/WIP/13/MISC/signinbg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="signinbg" tilewidth="32" tileheight="32" tilecount="2628" columns="73">
+ <image source="../../../../large splash art/signinbg.png" width="2354" height="1169"/>
+</tileset>
