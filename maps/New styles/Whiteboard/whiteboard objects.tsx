2826295f98ea82a3fbcb10d3f83c8a5e37ba9676
Sazdx <sazdxhikari@gmail.com>
2022-10-17 11:14:25 -0500

    Added whiteboard tilesheet and map test

---
+++ maps/New styles/Whiteboard/whiteboard objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="whiteboard objects" tilewidth="32" tileheight="32" tilecount="6450" columns="86">
+ <image source="whiteboard objects.png" width="2752" height="2400"/>
+</tileset>
