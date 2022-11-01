32d68ece4ced6b2487d981e7d4f1cc2871986328
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-11-01 09:33:16 -0400

    Added new profile heads wearables

---
+++ website/tilesheet_whiteboard_floors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tilesheet_whiteboard_floors" tilewidth="32" tileheight="32" tilecount="17856" columns="186">
+ <image source="tilesheet_whiteboard_floors.png" width="5952" height="3072"/>
+</tileset>
---
+++ website/tilesheet_whiteboard_terrains.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tilesheet_whiteboard_terrains" tilewidth="32" tileheight="32" tilecount="7840" columns="80">
+ <image source="tilesheet_whiteboard_terrains.png" width="2560" height="3136"/>
+</tileset>
