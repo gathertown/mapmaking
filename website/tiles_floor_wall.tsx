dbaa3d15896b981703487c23fbe72e7d6d90caa3
nostalgiapurgatory <info@nostalgiapurgatory.com>
2022-12-28 16:13:45 -0500

    Adds website assets
    WIP assets for the website

---
+++ website/tiles_floor_wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tiles_floor_wall" tilewidth="32" tileheight="32" tilecount="612" columns="36">
+ <image source="../maps/new styles/Professional Pixel/Tileset/tiles_floor_wall.png" width="1152" height="544"/>
+</tileset>
