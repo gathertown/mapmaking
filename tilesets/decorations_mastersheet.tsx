4df9c21f17b36f8984d0fee404c8237967c5cf4e
Sazdx <sazdxhikari@gmail.com>
2023-01-31 13:21:11 -0500

    added decorations and other object mastersheets to the tilesets

---
+++ tilesets/decorations_mastersheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Decorations Mastersheet" tilewidth="32" tileheight="32" tilecount="10000" columns="200">
+ <image source="Decorations Mastersheet.png" width="6400" height="1600"/>
+</tileset>
---
+++ tilesets/mastersheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="mastersheet" tilewidth="32" tileheight="32" tilecount="39000" columns="300">
+ <image source="Mastersheet.png" width="9600" height="4160"/>
+</tileset>
