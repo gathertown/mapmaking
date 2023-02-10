4fc379538d5ec79551cf15dc7b874330f4b4887f
Daud Alzayrt <dsalzayer@gmail.com>
2023-02-10 11:25:57 -0500

    UFP and Palate exploration

---
+++ maps/templates/UFP Offices/Assets/floors_3 complex colortest (grondpalate1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floors_3 complex colortest (grondpalate1)" tilewidth="32" tileheight="32" tilecount="17856" columns="186">
+ <image source="../../../../objects/WIP/DZ/Palates/floors_3 complex colortest (grondpalate1).png" width="5952" height="3072"/>
+</tileset>
---
+++ objects/WIP/DZ/Palates/gather_terrains_colortest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="gather_terrains_colortest" tilewidth="32" tileheight="32" tilecount="7840" columns="80">
+ <image source="gather_terrains_3 (grondpalate1).png" width="2560" height="3136"/>
+</tileset>
