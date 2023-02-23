cd8bcd3a2d4641280f139d76a401712a693e1555
Daud Alzayrt <dsalzayer@gmail.com>
2023-02-23 11:55:40 -0500

    Catchup
    Pushing neglected thigns that have been on my comptuer for too long
    - New UFP maps
    - Updates to old maps
    - File Cleanups, and restoring some files that have been accidentally deleted
    - Some tilesheet recolors

---
+++ maps/custom (internal)/Experiments/screenshot/gather_plants_outdoor_2.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="gather_plants_outdoor_2.1" tilewidth="32" tileheight="32" tilecount="5040" columns="40">
+ <image source="gather_plants_outdoor_2.1.png" width="1280" height="4032"/>
+</tileset>
---
+++ maps/templates/Office Standard (various)/6 person/flat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="flat" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="../../UFP Offices/Assets/flat.png" width="640" height="640"/>
+</tileset>
