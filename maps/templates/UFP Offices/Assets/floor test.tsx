2cdda6a84e08901b6297708c3b6933f27d0c51b8
Daud Alzayrt <dsalzayer@gmail.com>
2023-03-14 09:55:57 -0400

    ufp update

---
+++ maps/templates/UFP Offices/Assets/floor test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floor test" tilewidth="32" tileheight="32" tilecount="1344" columns="21">
+ <image source="floor test.png" width="672" height="2048"/>
+</tileset>
--- maps/templates/UFP Offices/Vanilla/10/floor test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="floor test" tilewidth="32" tileheight="32" tilecount="420" columns="21">
- <image source="../../Assets/floor test.png" width="672" height="640"/>
-</tileset>
