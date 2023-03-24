02bb757ddd621e2746ffa6d5b97079cffcb80912
Daud Alzayrt <dsalzayer@gmail.com>
2023-03-24 15:39:23 -0400

    UFP Megadose

---
+++ maps/custom (external)/EQT/.EQT Sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name=".EQT Sheet" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="EQT Sheet.png" width="640" height="640"/>
+</tileset>
---
+++ maps/custom (internal)/Experiments/screenshot/floors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floors" tilewidth="32" tileheight="32" tilecount="280" columns="28">
+ <image source="../../../../tilesets/Floors/floors.png" width="896" height="320"/>
+</tileset>
