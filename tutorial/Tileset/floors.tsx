0d420c22ed61863f48c2ced832dae186599ac2a2
Daud Alzayrt <dsalzayer@gmail.com>
2023-04-07 16:34:22 -0400

    Weekend

--- tutorial/Tileset/floors.tsx
+++ tutorial/Tileset/floors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="floors" tilewidth="32" tileheight="32" tilecount="1984" columns="32">
+<tileset version="1.5" tiledversion="1.7.2" name="Gather Floors" tilewidth="32" tileheight="32" tilecount="1984" columns="32">
  <image source="floors.png" width="1024" height="1984"/>
 </tileset>
