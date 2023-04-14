d235a4407c9be61d937fbb85dc890cc78243d039
Daud Alzayrt <dsalzayer@gmail.com>
2023-04-14 17:06:30 -0400

    Tilesheet progress

---
+++ tilesets/Floors/gather_floors_4.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Gather Floors 4.x" tilewidth="32" tileheight="32" tilecount="1240" columns="20">
+ <image source="floors_4.png" width="640" height="1984"/>
+</tileset>
