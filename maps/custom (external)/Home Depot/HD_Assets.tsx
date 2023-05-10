2bf6fb51a936e94582039f5bdfde5ad5ab7bd1ff
Daud Alzayrt <dsalzayer@gmail.com>
2023-05-10 16:37:40 -0400

    some nature recolors

---
+++ maps/custom (external)/Home Depot/HD_Assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="HD_Assets" tilewidth="32" tileheight="32" tilecount="50" columns="10">
+ <image source="Assets.png" width="320" height="160"/>
+</tileset>
