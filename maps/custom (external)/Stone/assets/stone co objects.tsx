0cd45a81c144fa53e1031b737fb0506d8e527bc3
Daud Alzayrt <dsalzayer@gmail.com>
2023-06-01 16:16:05 -0400

    objects and a map

---
+++ maps/custom (external)/Stone/assets/stone co objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="stone co objects" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="stone co objects.png" width="320" height="320"/>
+</tileset>
---
+++ tilesets/gather_walls_3.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="gather_walls_3.x" tilewidth="32" tileheight="32" tilecount="6384" columns="48">
+ <image source="walls_4.png" width="1536" height="4256"/>
+</tileset>
