8a984a7dee8f710612f757c043b08ef55875fdf3
Daud Alzayrt <dsalzayer@gmail.com>
2023-06-29 13:35:51 -0400

    misc

---
+++ maps/custom (external)/Payfit/_Roofs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Roofs" tilewidth="32" tileheight="32" tilecount="3744" columns="96">
+ <image source="../../../tilesets/gather_roofs_3.x.png" width="3072" height="1248"/>
+</tileset>
