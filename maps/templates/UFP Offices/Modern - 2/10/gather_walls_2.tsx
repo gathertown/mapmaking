462e6aa0dee47f6488bb3f14f3070b673b5d6efe
Daud Alzayrt <dsalzayer@gmail.com>
2023-07-18 16:09:01 -0400

    Custom office and UFP progress

---
+++ maps/templates/UFP Offices/Modern - 2/10/gather_walls_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="gather_walls_2" tilewidth="32" tileheight="32" tilecount="1750" columns="25">
+ <image source="../../../../../tilesets/Painter Beta Tool/gather_walls_2.png" width="800" height="2240"/>
+</tileset>
