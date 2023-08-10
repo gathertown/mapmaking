221e3cd031599166a33029fce5c5c7a8ff8e8028
nostalgiapurgatory <info@nostalgiapurgatory.com>
2023-08-10 14:44:25 -0400

    Adds new pets, computers, rugs

---
+++ objects/WIP/13/a TEMP folder/walltexture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="walltexture" tilewidth="32" tileheight="32" tilecount="1920" columns="24">
+ <image source="../../../../tilesets/Walls/walltexture.png" width="768" height="2560"/>
+</tileset>
