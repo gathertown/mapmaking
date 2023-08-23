e5b4b3686471b801ed96a01e264c1060cead4f59
Daud Alzayrt <dsalzayer@gmail.com>
2023-08-23 18:02:48 -0400

    So many UFP 3 maps

---
+++ tilesets/Gather-architecture-features.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Gather Architecture Features" tilewidth="32" tileheight="32" tilecount="3744" columns="48">
+ <image source="gather_architecture_features.png" width="1536" height="2496"/>
+</tileset>
---
+++ tilesets/gather_roofs_3.x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Gather Roofs 3.x" tilewidth="32" tileheight="32" tilecount="3744" columns="96">
+ <image source="gather_roofs_3.x.png" width="3072" height="1248"/>
+</tileset>
