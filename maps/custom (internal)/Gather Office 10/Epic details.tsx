d5fa1ccba4e38a45714adb7da80e4eb32fd53add
Daud Alzayrt <dsalzayer@gmail.com>
2023-09-20 14:48:13 -0400

    Gather office etc

---
+++ maps/custom (internal)/Gather Office 10/Epic details.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Epic details" tilewidth="32" tileheight="32" tilecount="504" columns="18">
+ <image source="epic details.png" width="576" height="896"/>
+</tileset>
