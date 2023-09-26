58d85d8aea3c4d72f0f83054359f93fcf1c21a62
Daud Alzayrt <dsalzayer@gmail.com>
2023-09-26 15:56:45 -0400

    LHH, Gather Office

---
+++ maps/custom (internal)/Gather Office 10/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="trees" tilewidth="32" tileheight="32" tilecount="2268" columns="27">
+ <image source="Trees.png" width="864" height="2688"/>
+</tileset>
