e99568ee73267a98a2ac2d133291dff6a60a78b5
Daud Alzayrt <dsalzayer@gmail.com>
2023-10-06 14:35:31 -0400

    Custom offices galore

--- tilesets/deprecated tilesheets/gather_floors_complex_3.tsx
+++ tilesets/deprecated tilesheets/gather_floors_complex_3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Gather Floors Complex 3" tilewidth="32" tileheight="32" tilecount="17856" columns="186">
- <image source="../Floors/floors_3_complex_S-20C-5.png" width="5952" height="3072"/>
+<tileset version="1.10" tiledversion="1.10.1" name="Gather Floors Complex 3" tilewidth="32" tileheight="32" tilecount="17856" columns="186">
+ <image source="Floors/floors_3_complex_S-20C-5.png" width="5952" height="3072"/>
 </tileset>
--- tilesets/deprecated tilesheets/gather_floors_simple_3.tsx
+++ tilesets/deprecated tilesheets/gather_floors_simple_3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Gather Floors Simple 3" tilewidth="32" tileheight="32" tilecount="6572" columns="124">
- <image source="../Floors/floors_3_simple_S-20C-5.png" width="3968" height="1696"/>
+<tileset version="1.10" tiledversion="1.10.1" name="Gather Floors Simple 3" tilewidth="32" tileheight="32" tilecount="6572" columns="124">
+ <image source="Floors/floors_3_simple_S-20C-5 b.png" width="3968" height="1696"/>
 </tileset>
