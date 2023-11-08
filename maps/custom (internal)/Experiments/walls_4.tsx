92ff11acc18b6dd33f0a522a7170199e57365a9a
Daud Alzayrt <dsalzayer@gmail.com>
2023-11-08 10:58:36 -0500

    latest custom office

--- maps/custom (internal)/Experiments/walls_4.tsx
+++ maps/custom (internal)/Experiments/walls_4.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="walls_4" tilewidth="32" tileheight="32" tilecount="7920" columns="48">
- <image source="../../../tilesets/walls_4.png" width="1536" height="5280"/>
+<tileset version="1.10" tiledversion="1.10.1" name="walls_4" tilewidth="32" tileheight="32" tilecount="3760" columns="40">
+ <image source="../../../tilesets/gather_floors_4.1.png" width="1280" height="3008"/>
 </tileset>
