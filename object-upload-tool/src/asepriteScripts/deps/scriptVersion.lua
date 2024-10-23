44f059587f63102c5ca1cc37ab44d19e115c6cae
Ryan Kubik <ryan@gather.town>
2024-10-23 11:04:58 -0700

    remove object-upload-tool

--- object-upload-tool/src/asepriteScripts/deps/scriptVersion.lua
+++
@@ -1,4 +0,0 @@
--- This file is generated.
--- It will be overwritten by bundleScripts.js during
--- the lua bundle process.
-return "1.0.0"
--- object-upload-tool/src/dev/watchSyncAsepriteScripts.js
+++
@@ -1,7 +0,0 @@
-import chokidar from "chokidar";
-import { localAsepriteScriptsDir } from "./config.js";
-import syncScriptDirectory from "./syncScriptDirectory.js";
-
-chokidar.watch(localAsepriteScriptsDir).on("all", (event, path) => {
-  syncScriptDirectory();
-});
